b0e4af6e79a1cfde61594a6e2692722d0268e1f5
Amit Kumar <aka883207@gmail.com>
2023-08-29 00:37:51 +0600

    29/08/23

--- UVa_Problems/test.cpp
+++ UVa_Problems/test.cpp
@@ -4,21 +4,21 @@ int main()
 {
     freopen("input.txt", "r", stdin);
     int t;
-    while (cin>>t and t!=0)
+    while (1)
     {
-        
-        while(t--){
-        int n,x,y;
-        cin>>n>>x>>y;
-            while(n--){ int a,b;
-                cin>>a>>b;
-                if(a==x or a==y or b==x or b==y) cout<<"divisa"<<endl;
-                else if(a<0 and b<0)cout<<"SO"<<endl;
-                else if(a>0 and b>0) cout<<"NE"<<endl;
-                else if(a>0 and b<0) cout<<"NO"<<endl;
-                else if(a<0 and b>0) cout<<"SE"<<endl;
-            }
+        cin>>t;
+        if(t==0) break;
+        int x,y;
+        cin>>x>>y;
+        while(t--){ int a,b;
+            cin>>a>>b;
+            if(a==x or a==y or b==x or b==y) cout<<"divisa"<<endl;
+            else if(a<x and b<y)cout<<"SO"<<endl;
+            else if(a>x and b>y) cout<<"NE"<<endl;
+            else if(a>x and b<y) cout<<"SE"<<endl;
+            else if(a<x and b>y) cout<<"NO"<<endl;
         }
+        
     }
     
     return 0;
