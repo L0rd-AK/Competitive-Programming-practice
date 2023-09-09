0394f3e1bbe234db609e92a726d7b39c6ed9c098
Amit Kumar <aka883207@gmail.com>
2023-09-10 02:28:10 +0600

    10/09/23

---
+++ CodeForces-Problems/102263M.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+
+int main() 
+{
+  freopen("D:\\VS-Code\\Competitive programming practice\\input.txt","r",stdin);
+  queue<string> q;
+  string s;
+  cin>>s;
+  sort(s.begin(),s.end());
+  for(int i=s.length()-1;i>=0;i--){
+    if(s[i]=='z' and s[i-1]=='z'){
+        
+    }
+  }
+ return 0;
+}
