1ee05e1278075c2463fd1a44c843fe14ff88831c
Amit Kumar <aka883207@gmail.com>
2023-10-27 02:44:39 +0600

    27/10/23

--- topH/ass.cpp
+++ topH/ass.cpp
@@ -7,21 +7,11 @@ using namespace std;
 int main(){
     freopen("D:\\VS-Code\\Competitive programming practice\\input.txt","r",stdin);
     //freopen("D:\\VS-Code\\Competitive programming practice\\output.txt","w",stdout);
-    ll x,y;
-    cin>>x>>y;
-    ll n=y-x;
-    if(n/32) n=n%32;
-    if(n==2){cout<<"Yes\n";return 0;}
-    else if(n<2){cout<<"No\n";return 0;}
-    if(n/16) n=n%16;
-    if(n==2){cout<<"Yes\n";return 0;}
-    else if(n<2){cout<<"No\n";return 0;}
-    if(n/8) n=n%8;
-    if(n==2){cout<<"Yes\n";return 0;}
-    else if(n<2){cout<<"No\n";return 0;}
-    if(n/4) n=n%4;
-    if(n>=2){cout<<"Yes\n";return 0;}
-    else if(n<2){cout<<"No\n";return 0;}
+    int x,z,y,m;
+    cin>>x>>z>>y>>m;
+    if(x<=y)cout<<x*z-y*m<<'\n';
+    else cout<<x*z<<'\n';
+    
     
     return 0;
 }
