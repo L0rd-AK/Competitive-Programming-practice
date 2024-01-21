2cb5376789aa0aa76c582b11bdfa61b8cc7b7f29
Amit Kumar <aka883207@gmail.com>
2024-01-22 01:27:39 +0600

    22/01/24

--- CodeForces-Problems/1669A.cpp
+++ CodeForces-Problems/1669A.cpp
@@ -10,11 +10,13 @@ int main()
 	int t;
        cin>>t;
        while(t--){
-              int a,b,c;
-              cin>>a>>b>>c;
-
-              if(a-b==c)cout<<"-\n";
-              else cout<<"+\n";
+              int x;
+              cin>>x;
+              if(x<=1399)cout<<"Division 4\n";
+              else if(x>=1400 and x<=1599)cout<<"Division 3\n";
+              else if(x>=1600 and x<=1899)cout<<"Division 2\n";
+              else if(x>=1900)cout<<"Division 1\n";
        }
+       
 	return 0;
 }
