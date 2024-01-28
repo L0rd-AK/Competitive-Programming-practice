1e050936b6e5cd16fcc37dd022d49f068b387f45
Amit Kumar <aka883207@gmail.com>
2024-01-28 22:13:56 +0600

    28/01/24

--- CodeForces-Problems/1554A.cpp
+++ CodeForces-Problems/1554A.cpp
@@ -11,8 +11,16 @@ int main()
    int t;
    cin>>t;
    while(t--){
-      ll n;
+      int n;
       cin>>n;
+      ll a[n],mx=INT_MIN,idx=-1;
+      for(int i=0;i<n;i++) cin>>a[i];
+      ll ans=INT_MIN;
+      for(int i=1;i<n;i++){
+         if(ans<a[i]*a[i-1])ans=a[i]*a[i-1];
+      }
+      cout<<ans<<endl;
+      
       
    }
     
