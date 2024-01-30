f2612adc94cdd6e33d71de834e863a4222a2c683
Amit Kumar <aka883207@gmail.com>
2024-01-30 22:19:32 +0600

    30/01/24

--- Zest-cases/vjudge.cpp
+++ Zest-cases/vjudge.cpp
@@ -7,17 +7,29 @@ int main()
    #ifndef ONLINE_JUDGE
       freopen("D:\\VS-Code\\Competitive programming practice\\input.txt", "r", stdin);
    #endif
-
    int t;
    cin>>t;
    while(t--){
-      int n,x;
-      cin>>x>>n;
-      int z=x/n;
-      cout<<z<<" "<<x-(n*(z-1))<<endl;
-    //   cout<<__gcd(z,x-(z*(n-1)))<<'\n';
-      
+      int n;
+      cin>>n;
+      int a[n],b[n];
+      for(int i=0;i<n;i++)cin>>a[i];
+      for(int i=0;i<n;i++)cin>>b[i];
+      vector< pair <int,int> > v;
+      for(int i=0;i<n;i++){
+         v.push_back(make_pair(b[i],a[i]));
+      }
+      sort(v.begin(),v.end());
+      for(int i=0;i<n;i++){
+         cout<<v[i].second<<" ";
+      }cout<<'\n';
+      for(int i=0;i<n;i++){
+         cout<<v[i].first<<" ";
+      }cout<<'\n';
+
    }
+   
+
     
 	return 0;
 }
