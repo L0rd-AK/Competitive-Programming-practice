fd57f8473f154383e7112d320558db99cc56527a
Amit Kumar <aka883207@gmail.com>
2024-02-02 19:34:55 +0600

    02/02/24

--- CodeForces-Problems/1850D.cpp
+++ CodeForces-Problems/1850D.cpp
@@ -1,8 +1,8 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define ll long long int
-#define f(x1,y1,z1) for(int x1=y1;x1<z1;x1++)
-#define f1(x1,y1,z1) for(int x1=y1;x1<=z1;x1++)
+#define f(x1,y1,z1) for(ll x1=y1;x1<z1;x1++)
+#define f1(x1,y1,z1) for(ll x1=y1;x1<=z1;x1++)
 int main()
 {
    #ifndef ONLINE_JUDGE
@@ -11,12 +11,20 @@ int main()
    int t;
    cin>>t;
    while(t--){
-      int n,ful,a,b;
-      cin>>n>>ful>>a>>b;
-      int arr[n];
-      f(i,0,n)cin>>arr[i];
-      if((ful/b)>=n)cout<<"YES\n";
-      else cout<<"NO\n";
+      int n,k;
+      cin>>n>>k;
+      if(n==1)cout<<k<<'\n';
+      else if(n==k)cout<<1<<'\n';
+      else if(k>n){
+         int x=k/n;
+         int y=k%n;
+         if(y!=0)x++;
+         cout<<x<<'\n';
+      }else{
+         int x=(n+k-1)/k;
+         ll y=((1LL*x*k)+n-1)/n;
+         cout<<y<<'\n';
+      }
 
    }
    
