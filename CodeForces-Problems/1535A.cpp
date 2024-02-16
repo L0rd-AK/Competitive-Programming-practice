998978e08cc4f7d43a0a269e47097ea32c28db59
Amit Kumar <aka883207@gmail.com>
2024-02-16 16:31:43 +0600

    16/02/24

---
+++ CodeForces-Problems/1535A.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define f(x1,y1,z1) for(int x1=y1;x1<z1;x1++)
+#define f1(x1,y1,z1) for(int x1=y1;x1<=z1;x1++)
+int main()
+{
+   #ifndef ONLINE_JUDGE
+      freopen("D:\\VS-Code\\Competitive programming practice\\input.txt", "r", stdin);
+   #endif
+      int t;
+      cin>>t;
+      while(t--){
+      int a,b,c,d;
+      cin>>a>>b>>c>>d;
+      if (min(a, b) > max(c, d) || max(a, b) < min(c, d))cout << "NO\n";
+      else cout << "YES\n";
+
+      
+   }
+   
+   return 0;
+}
