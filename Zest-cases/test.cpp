3f37a3836465447cddf7e92762cce22dcb0f7d4f
Amit Kumar <aka883207@gmail.com>
2024-02-23 16:20:50 +0600

    23/02/24

--- Zest-cases/test.cpp
+++ Zest-cases/test.cpp
@@ -13,16 +13,37 @@ int main()
       int t;
       cin>>t;
       while(t--){
-         int n;
-         cin>>n;
-         int sum=0;
-         f(i,0,n){
-            int x;
-            cin>>x;
-            sum+=x;
+         int n; cin>>n;
+         string s; cin>>s;
+         int zeros=0;
+         stack<int> zero_pos,one_pos;
+         for(int i=0;i<n;i++){
+            if(s[i]=='0'){
+               zeros++;
+               zero_pos.push(i+1);
+            }else one_pos.push(i+1);
          }
-         if(sum%2 == 0)cout<<"YES\n";
-         else cout<<"NO\n";
+         // printf("%d %d %d\n",zeros,zero_pos.size(),one_pos.size());
+        
+         int z=0;
+         for(int i=n-1;i>=0;i--){
+            if(i+1 < zeros)cout<<-1<<" ";
+            else if(s[i]=='0')cout<<0<<" ";
+            else{
+               int pos1=one_pos.top();
+               one_pos.pop();
+               int pos0;
+               while(1){
+                  pos0=zero_pos.top();
+                  zero_pos.pop();
+                  if(pos1>pos0)break;
+               }
+               z+=abs(pos0-pos1);
+               swap(s[pos0-1],s[pos1-1]);
+               cout<<z<<" ";
+            }
+         }
+         cout<<'\n';
       }
    
    return 0;
