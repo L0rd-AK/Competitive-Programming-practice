a26ac8057425830cf3188d0104f282ec1e2a1811
Amit Kumar <aka883207@gmail.com>
2024-03-10 01:19:26 +0600

    10/03/24

--- Zest-cases/test.cpp
+++ Zest-cases/test.cpp
@@ -1,30 +1,39 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define ll long long int
-#define f(x1,y1,z1) for(int x1=y1;x1<z1;x1++)
-#define f1(x1,y1,z1) for(int x1=y1;x1<=z1;x1++)
+#define f(x1, y1, z1) for (int x1 = y1; x1 < z1; x1++)
+#define f1(x1, y1, z1) for (int x1 = y1; x1 <= z1; x1++)
 
 int main()
 {
-   #ifndef ONLINE_JUDGE
-      freopen("D:\\VS-Code\\Competitive programming practice\\input.txt", "r", stdin);
-   #endif
-      
-      int t;cin>>t;
-      while(t--){
-         int n;cin>>n;
-         int a[n];
-         f(i,0,n)cin>>a[i];
-         sort(a,a+n);
-         bool flag=1;
-         for(int i=0;i<n-2;i++){
-            if(a[i]==a[i+2]){
-               cout<<a[i]<<'\n';flag=0;
-               break;
-            }
+#ifndef ONLINE_JUDGE
+   freopen("D:\\VS-Code\\Competitive programming practice\\input.txt", "r", stdin);
+#endif
+
+   int t;
+   cin >> t;
+   while (t--)
+   {
+      int a, b, n;
+      cin >> a >> b >> n;
+      ll x = (a + b), ans = 1;
+      if(a<b){
+         while(x<=n){
+            a=b;
+            b=x;
+            x=(a+b);
+            ans++;
          }
-         if(flag)cout<<-1<<'\n';
+      }else{
+         while(x<=n){
+            b=a;
+            a=x;
+            x=(a+b);
+            ans++;
+         }
       }
-      
+      cout << ans << endl;
+   }
+
    return 0;
 }
