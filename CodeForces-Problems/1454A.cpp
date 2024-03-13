cb17d60834674ea93435a4070387b1479e47f43a
Amit Kumar <aka883207@gmail.com>
2024-03-14 05:14:11 +0600

    14/03/24

--- CodeForces-Problems/1454A.cpp
+++ CodeForces-Problems/1454A.cpp
@@ -3,6 +3,7 @@ using namespace std;
 #define ll long long int
 #define f(x1, y1, z1) for (int x1 = y1; x1 < z1; x1++)
 #define f1(x1, y1, z1) for (int x1 = y1; x1 <= z1; x1++)
+
 int main()
 {
 #ifndef ONLINE_JUDGE
@@ -14,10 +15,9 @@ int main()
    while (t--)
    {
       int n;cin>>n;
-      ll s1=pow(2,n),s2=0;
-      for(int i=1;i<n/2;i++) s1+=pow(2,i);
-      for(int i=n/2;i<n;i++) s2+=pow(2,i);
-      cout<<s1-s2<<endl;
+      cout<<n<<" ";
+      f(i,1,n)cout<<i<<" ";
+      cout<<'\n';
    }
 
    return 0;
