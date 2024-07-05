519ecfb5caee3ea29ee24e7d368edf067e886398
l0rd-AK <aka883207@gmail.com>
2024-07-05 23:56:20 +0600

    05/07/24

--- Zest-cases/test.cpp
+++ Zest-cases/test.cpp
@@ -12,31 +12,33 @@ int main()
    int t;
    cin>>t;
    while(t--){
-      int n;
-      cin>>n;
-      ll a[n],b[n];
-      vector<ll> dif;
-      f(i,0,n){cin>>a[i];b[i]=a[i];}
-      f(i,1,n){
-         if(b[i]<b[i-1]){
-            b[i]=b[i-1];
+      int n,f,k;
+      cin>>n>>f>>k;
+      int a[n];
+      f(i,0,n)cin>>a[i];
+      int z=a[f-1];
+      if(k==n){cout<<"YES\n";continue;}
+      
+      sort(a,a+n,greater<int>());
+      bool flg=1;
+      if(a[k]==z and a[k-1]==z and k!=n){cout<<"MAYBE\n";flg=0;}
+      if(flg)
+      f(i,0,k){
+         if(a[i]==z){
+            cout<<"YES\n";
+            flg=0;
+            break;
          }
       }
-      f(i,0,n){
-         int x=b[i]-a[i];
-         if(x)dif.push_back(x);
-      }
-      int size=dif.size();
-      sort(dif.begin(),dif.end());
+      
+      if(flg)cout<<"NO\n";
 
-      ll ans=0,z=0,zom=0;
-      f(i,0,size){
-         z+=dif[i];
-         zom=dif[i];
-         ans+=(zom*(size+1-i));
-         if(i != size-1)dif[i+1]=(dif[i+1]-z);
-      }
-      cout << ans << endl;
+      /*
+      cout<<z<<endl;
+      cout<<a[k]<<endl;
+      */
+      
+
    }
 
     return 0;
