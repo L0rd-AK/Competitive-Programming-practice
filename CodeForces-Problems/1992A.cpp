b13b5328204df544951866c3283297b37ada1983
l0rd-AK <aka883207@gmail.com>
2024-07-13 10:49:10 +0600

    13/07/24

--- CodeForces-Problems/1992A.cpp
+++ CodeForces-Problems/1992A.cpp
@@ -13,11 +13,16 @@ int main()
     int t;
     cin >> t;
     while (t--) {
-        int n,m,k;
-        cin>>n>>m>>k;
-        string s;
-        cin>>s;
-
+        int a[3];
+        cin>>a[0]>>a[1]>>a[2];
+        sort(a,a+3);
+        int five=5;
+        while(five){
+            a[0]++;
+            five--;
+            sort(a,a+3);
+        }
+        cout<<a[0]*a[1]*a[2]<<endl;
     }
     
     return 0;
