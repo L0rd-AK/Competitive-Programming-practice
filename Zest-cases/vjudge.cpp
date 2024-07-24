ff5c7926ebae7c71ee6390a59059784d3be1a318
l0rd-AK <aka883207@gmail.com>
2024-07-25 00:06:21 +0600

    25/07/24

--- Zest-cases/vjudge.cpp
+++ Zest-cases/vjudge.cpp
@@ -1,6 +1,5 @@
 #include <bits/stdc++.h>
 using namespace std;
-
 #define ll long long int
 #define f(x1, y1, z1) for (int x1 = y1; x1 < z1; x1++)
 #define f1(x1, y1, z1) for (int x1 = y1; x1 <= z1; x1++)
@@ -8,34 +7,35 @@ using namespace std;
 #define yes cout<<"YES"<<endl
 #define no cout<<"NO"<<endl
 
+void AKG(){
+    ll n,k;
+    cin>>n>>k;
+    if(k<n)cout<<k<<endl;
+    else if(k==n)cout<<k+1<<endl;
+    else{
+        ll x=(k-1)/(n-1);
+        cout<<k+x<<endl;
+    }
+    
+}
+
 int main()
 {
 #ifndef ONLINE_JUDGE
     freopen("D:\\VS-Code\\Competitive programming practice\\input.txt", "r", stdin);
+    auto begin = std::chrono::high_resolution_clock::now();
 #endif
-    
-    int n;
-    int a[3];
-    cin>>n>>a[0]>>a[1]>>a[2];
-    vector<vector<ll>>dp(5,vector<ll>(n+2));
-    f(i,0,5){
-        f(j,0,n+2){
-            dp[i][j]=INT_MIN;
-        }
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int t;
+    cin>>t;
+    while(t--){
+        AKG();
     }
-    f(i,0,5) {dp[i][0]=0;}
-    // calc
-    f(i,1,4){
-        f(j,1,n+1){
-            if(a[i-1]>j) {dp[i][j]=dp[i-1][j];}
-            else{
-                dp[i][j]=max(dp[i-1][j],1+dp[i][j-a[i-1]]);
-            }
-        }
-    }
-    cout<<dp[3][n]<<endl;
-
-
-    
+#ifndef ONLINE_JUDGE
+    auto end = std::chrono::high_resolution_clock::now();
+    auto elapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(end - begin);
+    cerr << "Time measured: " << elapsed.count() * 1e-9 << " seconds.\n"; 
+#endif
     return 0;
 }
