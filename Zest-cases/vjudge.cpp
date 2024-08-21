35cb947810b9fe760bdd3582fac365d499029575
l0rd-AK <aka883207@gmail.com>
2024-08-21 15:56:03 +0600

    21/08/24

--- Zest-cases/vjudge.cpp
+++ Zest-cases/vjudge.cpp
@@ -1,38 +1,50 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
+#define ll long long int
+#define f(x1, y1, z1) for (int x1 = y1; x1 < z1; x1++)
+#define f1(x1, y1, z1) for (int x1 = y1; x1 <= z1; x1++)
+#define endl "\n"
+#define yes cout<<"YES"<<endl
+#define no cout<<"NO"<<endl
+#define prnt(x) cout<<x<<endl
+
+void AKG(){
+    int n,s,m;
+    cin>>n>>s>>m;
+    bool flg=0;
+    int a,b,x,y;
+    cin>>a>>b;n--;
+    x=a;
+    y=b;
+    if(a>=s)flg=1;
+    while(n--){
+        cin>>a>>b;
+        if(a-y>=s)flg=1;
+        x=a;
+        y=b;
+    }
+    if(m-b>=s)flg=1;
+    if(flg)yes;
+    else no;
+}
 
 int main()
 {
-   
-    int m, s, t, i;
-    cin >> m >> s;
-    string a, b;
-
-    if (s == 0)
-    {
-        cout << (m == 1 ? "0 0" : "-1 -1") << endl;
-        return 0;
+#ifndef ONLINE_JUDGE
+    freopen("D:\\VS-Code\\Competitive programming practice\\input.txt", "r", stdin);
+    auto begin = std::chrono::high_resolution_clock::now();
+#endif
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int t=1;
+    cin>>t;
+    while(t--){
+        AKG();
     }
-
-    for (i = 0; i < m; i++)
-    {
-        t = min(s, 9);
-        b += t + '0';
-        s -= t;
-    }
-
-    if (s > 0)
-    {
-        cout << "-1 -1" << endl;
-        return 0;
-    }
-
-    for (i = m - 1; i >= 0; i--)
-        a += b[i];
-
-    for (i = 0; a[i] == '0'; i++);
-
-    a[i]--, a[0]++;
-    cout << a << " " << b << endl;
+#ifndef ONLINE_JUDGE
+    auto end = std::chrono::high_resolution_clock::now();
+    auto elapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(end - begin);
+    cerr << "Time measured: " << elapsed.count() * 1e-9 << " seconds.\n"; 
+#endif
     return 0;
 }
