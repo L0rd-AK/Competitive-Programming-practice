bec76f15ab4ee8ed2f11ade787ede57a82d33dce
l0rd-AK <aka883207@gmail.com>
2024-10-17 23:37:04 +0600

    17/10/24

---
+++ CodeForces-Problems/2025C.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long int
+#define f(x1, y1, z1) for (int x1 = y1; x1 < z1; x1++)
+#define f1(x1, y1, z1) for (int x1 = y1; x1 <= z1; x1++)
+#define endl "\n"
+#define yn(f) f ? cout << "YES\n" : cout << "NO\n"
+#define prnt(x) cout << x << endl
+
+void AKG() {
+    int n, k;
+    cin >> n >> k;
+    vector<int> a(n);
+    for (int i = 0; i < n; ++i) {
+        cin >> a[i];
+    }
+    
+    sort(a.begin(), a.end());
+    
+    int ans = 0;
+    int j = 0;
+    for (int i = 0; i < n; ++i) {
+        j = max(i, j);
+        while (j + 1 < n && a[j + 1] - a[j] <= 1 && a[j + 1] - a[i] < k) {
+            j++;
+        }
+        ans = max(ans, j - i + 1);
+    }
+    
+    prnt(ans);
+}
+int main() {
+    #ifndef ONLINE_JUDGE
+        freopen("D:\\VS-Code\\Competitive programming practice\\input.txt", "r", stdin);
+        auto begin = std::chrono::high_resolution_clock::now();
+    #endif
+
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    
+    int t;
+    cin >> t;
+    while (t--) {
+        AKG();
+    }
+
+    #ifndef ONLINE_JUDGE
+        auto end = std::chrono::high_resolution_clock::now();
+        auto elapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(end - begin);
+        cerr << "Time measured: " << elapsed.count() * 1e-9 << " seconds.\n";
+    #endif
+    
+    return 0;
+}
