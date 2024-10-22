06fa73de23f5dc57628c5d1103487e2f8a5e55c2
l0rd-AK <aka883207@gmail.com>
2024-10-22 12:23:04 +0600

    22/10/24

--- Zest-cases/ass.cpp
+++ Zest-cases/ass.cpp
@@ -1,67 +1,35 @@
-#include <iostream>
-#include <vector>
-#include <numeric>
-#include <cmath>
+#include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
 
-// Function to calculate the best sum close to half the total sum with exactly 'k' coins
-int bestSubsetSum(vector<int>& coins, int N, int k, int totalSum) {
-    int targetSum = totalSum / 2; // We want to get as close as possible to half the total sum
 
-    // DP table to store whether a specific sum with exactly j coins is possible
-    vector<vector<bool>> dp(k + 1, vector<bool>(targetSum + 1, false));
-    dp[0][0] = true;  // A sum of 0 is always possible with 0 coins
+void solve(){
+    int n,x;cin>>n>>x;
+    int arr[n];
 
-    // DP calculation
-    for (int i = 0; i < N; ++i) {
-        int coinValue = coins[i];
-        for (int j = k; j > 0; --j) {  // Loop for exactly 'k' coins
-            for (int s = targetSum; s >= coinValue; --s) {
-                dp[j][s] = dp[j][s] || dp[j - 1][s - coinValue];
-            }
-        }
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    int freq[n+1] = {0};
+    for(int i=0;i<n;i++){
+        if(arr[i]<n+1) freq[arr[i]]++;
     }
 
-    // Find the closest sum to targetSum
-    for (int s = targetSum; s >= 0; --s) {
-        if (dp[k][s]) {
-            return s;
+    for(int i=0;i<n+1;i++){
+        if(freq[i]==0){
+            cout<<i<<endl;
+            return;
         }
+        else if(freq[i]>1){
+            if(i+x<=n) freq[i+x]+=freq[i]-1;
+        }
+        //cout<<freq[i]<<" ";
     }
 
-    return 0;
+    
 }
 
-int minDifference(vector<int>& coins, int N) {
-    int totalSum = accumulate(coins.begin(), coins.end(), 0);  // Total sum of coins
-
-    int halfSize = N / 2;
-    int bestSum1 = bestSubsetSum(coins, N, halfSize, totalSum);
-
-    if (N % 2 == 0) {
-        // If N is even, both subsets have N/2 coins
-        return abs(totalSum - 2 * bestSum1);
-    } else {
-        // If N is odd, find the best sum with N/2 coins and (N/2) + 1 coins
-        int bestSum2 = bestSubsetSum(coins, N, halfSize + 1, totalSum);
-        return min(abs(totalSum - 2 * bestSum1), abs(totalSum - 2 * bestSum2));
-    }
-}
-
-int main() {
-    int T;
-    cin >> T;
-
-    while (T--) {
-        int N;
-        cin >> N;
-        vector<int> coins(N);
-        for (int i = 0; i < N; ++i) {
-            cin >> coins[i];
-        }
-
-        cout << minDifference(coins, N) << endl;
-    }
-
-    return 0;
+int main(){
+    int t;cin>>t;
+    while(t--) solve();
 }
--- Zest-cases/vjudge.cpp
+++ Zest-cases/vjudge.cpp
@@ -7,34 +7,45 @@ using namespace std;
 #define endl "\n"
 #define yn(f) f ? cout << "YES\n" : cout << "NO\n"
 #define prnt(x) cout << x << endl
- 
-void AKG() {
-    ll n;
-    cin >> n;
-    cout << n + int(sqrtl(n) + 0.5) << "\n";
-}
 
+void AKG() {
+    int n;cin>>n;
+    string a,b;
+    cin>>a>>b;
+    int ans1=0;
+        int x=0,y=0;
+    for(int i=0;i<n;i+=3){
+        x=0,y=0;
+        for(int j=i;j<3;j++){
+            if(a[i]=='A')x++;
+            if(b[i]=='A')y++;
+        }
+        if(x>=2)ans1++;
+        if(y>=2)ans1++;
+    }
+    cout<<x<<" "<<y<<" "<<ans1<<endl;
+    prnt(ans1);
+}
 int main() {
-#ifndef ONLINE_JUDGE
-    freopen("D:\\VS-Code\\Competitive programming practice\\input.txt", "r", stdin);
-    auto begin = std::chrono::high_resolution_clock::now();
-#endif
+    #ifndef ONLINE_JUDGE
+        freopen("D:\\VS-Code\\Competitive programming practice\\input.txt", "r", stdin);
+        auto begin = std::chrono::high_resolution_clock::now();
+    #endif
 
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     
     int t;
     cin >> t;
-    
     while (t--) {
         AKG();
     }
 
-#ifndef ONLINE_JUDGE
-    auto end = std::chrono::high_resolution_clock::now();
-    auto elapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(end - begin);
-    cerr << "Time measured: " << elapsed.count() * 1e-9 << " seconds.\n";
-#endif
-
+    #ifndef ONLINE_JUDGE
+        auto end = std::chrono::high_resolution_clock::now();
+        auto elapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(end - begin);
+        cerr << "Time measured: " << elapsed.count() * 1e-9 << " seconds.\n";
+    #endif
+    
     return 0;
 }
