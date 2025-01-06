00edf2c493fd90b76439b3f0ea7495d35f205455
l0rd-AK <aka883207@gmail.com>
2025-01-06 22:54:36 +0600

    I am tired of being mediocre.

---
+++ XPSC 05/Week-11/Day-2/Array-Fix.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define f(x1, y1, z1) for (int x1 = y1; x1 < z1; x1++)
+#define f1(x1, y1, z1) for (int x1 = y1; x1 <= z1; x1++)
+
+int main()
+{
+#ifndef ONLINE_JUDGE
+   freopen("D:\\VS-Code\\Competitive programming practice\\input.txt", "r", stdin);
+#endif
+
+   int t;cin>>t;
+   while(t--){
+      int n;cin>>n;
+      int a[n];
+      f(i,0,n)cin>>a[i];
+      vector<int> v({a[n-1]});
+      for (int i = n - 2; i >= 0; --i) {
+      if (a[i] > v.back()) {
+        v.push_back(a[i] % 10);
+        v.push_back(a[i] / 10);
+      } else {
+        v.push_back(a[i]);
+      }
+    }
+
+      if(is_sorted(v.rbegin(),v.rend()))cout<<"YES\n";
+      else cout<<"NO\n";
+   }
+   return 0;
+}
