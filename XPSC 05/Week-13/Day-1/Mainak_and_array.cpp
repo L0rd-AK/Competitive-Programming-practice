119d5a39e6c0c837deb085ec3260ec849c6650a2
l0rd-AK <aka883207@gmail.com>
2025-01-24 23:56:48 +0600

    a losers way to the top

--- XPSC 05/Week-13/Day-1/Mainak_and_array.cpp
+++ XPSC 05/Week-13/Day-1/Mainak_and_array.cpp
@@ -57,24 +57,22 @@ void _print(T t, V... v)
 #endif
 
 void AKG(){
-   int n, a, b;
-        cin >> n >> a >> b;
-        vector<int> p;
-        p.push_back(a);
-        for (int j = n; j > 0; --j) {
-            if (j != a && j != b) {
-                p.push_back(j);
-            }
-        }
-        p.push_back(b);
-        if (p.size() == n && *min_element(p.begin(), p.begin() + n / 2) == a && *max_element(p.begin() + n / 2, p.end()) == b) {
-            for (int x : p) {
-                cout << x << " ";
-            }
-            cout << endl;
-        } else {
-            cout << -1 << endl;
-        }
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    f(i, 0, n) cin >> a[i];
+    int ans = 0;
+    f(i,0,n-1){
+        ans=max(ans,(a[i]-a[i+1]));
+    }
+    ans=max(ans,(a[n-1]-a[0]));
+    f(i,1,n){
+        ans=max(ans,(a[i]-a[0]));
+    }
+    f(i,0,n-1){
+        ans=max(ans,(a[n-1]-a[i]));
+    }
+    prnt(ans);
 }
 
 int main()
