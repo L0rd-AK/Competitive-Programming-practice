7af6bb7ba550464bce53a0eebe1b8bd782d44763
l0rd-AK <aka883207@gmail.com>
2025-02-26 23:47:46 +0600

    reached to pupil

--- Zest-cases/ass.cpp
+++ Zest-cases/ass.cpp
@@ -1,40 +1,74 @@
-#include <iostream>
-#include <algorithm>
+#include <bits/stdc++.h>
 using namespace std;
 
-typedef unsigned int uint;
-typedef long long ll;
+#define ll long long
+#define f(x1, y1, z1) for (int x1 = y1; x1 < z1; x1++)
+#define endl "\n"
+#define yn(f) cout << (f ? "YES\n" : "NO\n")
+#define pr(x) cout << x << endl
+#define all(x) x.begin(), x.end()
+#define vl vector<ll>
+#define vi vector<int>
+#define pb push_back
+#define pi pair<ll, ll>
 
-int main(){
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-    freopen("D:\\VS-Code\\Competitive programming practice\\input.txt", "r", stdin);
-    
-    int t;
-    cin >> t;
-    while(t--){
-        uint l, r;
-        cin >> l >> r;
-        uint count = r - l + 1;
-        uint window = (count < 128 ? count : 128);
-        uint start = (count < 128 ? l : (r - window + 1));
-        
-        ll bestVal = -1;
-        uint bestA = 0, bestB = 0, bestC = 0;
-        
-        for (uint a = start; a <= r; a++){
-            for (uint b = a + 1; b <= r; b++){
-                for (uint c = b + 1; c <= r; c++){
-                    ll cur = ((ll)a ^ b) + ((ll)a ^ c) + ((ll)b ^ c);
-                    if(cur > bestVal){
-                        bestVal = cur;
-                        bestA = a; bestB = b; bestC = c;
-                    }
-                }
-            }
+const int MOD = 998244353;
+const int N = 3e5+5;
+
+// Returns the index of the lowest unset bit in x
+static int stBit(ll x) {
+    for (int b = 0; b < 30; b++) {
+        if (((x >> b) & 1) == 0) {
+            return b;
         }
-        cout << bestA << " " << bestB << " " << bestC << "\n";
     }
+    return 30; 
+}
+
+void AKG() {
+    ll n, x;
+    cin >> n >> x;
+    int b = stBit(x);  // Find smallest missing bit in x
+    ll m = (1LL << b); // 2^b
+    vl ans;
     
+    if (n >= m + 1) {
+        // Case when we have enough slots to include {0, 1, ..., 2^b - 1} and x
+        for (int i = 0; i < m; i++) {
+            ans.push_back(i);
+        }
+        ans.push_back(x);
+        while ((int)ans.size() < n) {
+            ans.push_back(0);
+        }
+    } else {
+        // Case when we cannot fit all {0, ..., 2^b-1}
+        ll val = 0;
+        for (int i = 0; i < n; i++) {
+            ans.push_back(i);
+            val |= i;
+        }
+        if (val != x) {
+            // Adjust the last element to make OR equal to x
+            ans[n - 1] = x ^ val;
+        }
+    }
+
+    for (int i = 0; i < n; i++) {
+        cout << ans[i] << (i + 1 < n ? ' ' : '\n');
+    }
+}
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    freopen("D:\\VS-Code\\Competitive programming practice\\input.txt", "r", stdin);
+
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        AKG();
+    }
+
     return 0;
 }
