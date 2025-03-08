39474de2286779c45440d5d9c4e14e7fd83f2185
l0rd-AK <aka883207@gmail.com>
2025-03-08 23:11:23 +0600

    My enemies are many, my equals are none

---
+++ LeetCode/Three_consecutive_odds.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+    public:
+        bool threeConsecutiveOdds(vector<int>& arr) {
+            bool ans=false;
+            int n=arr.size();
+            for(int i=2;i<n;i++){
+                if(arr[i-2]%2==1&&arr[i-1]%2==1&&arr[i]%2==1){
+                    ans=true;
+                    break;
+                }
+            }
+            return ans;
+        }
+    };
