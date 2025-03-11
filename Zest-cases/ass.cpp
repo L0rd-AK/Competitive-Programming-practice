d2cb38e0ad30aadb7f1dfdb29022bf886b919057
l0rd-AK <aka883207@gmail.com>
2025-03-11 23:47:16 +0600

    if you could have done it, you would have done it

--- Zest-cases/ass.cpp
+++ Zest-cases/ass.cpp
@@ -1,10 +1,63 @@
 #include <bits/stdc++.h>
 using namespace std;
+
 int main() {
-    int t = 1;cin >> t;
-    while (t--) {int n,d,k;cin>>n>>d>>k;int a[n+2]={0};
-        for(int i=1;i<=k;i++){int x,y;cin>>x>>y;a[max(1,x-d+1)]++;a[y+1]--;}int x=1,y=1;
-        for(int i=1;i<=n-d+1;i++){a[i]+=a[i-1];if(a[i]>a[x])x=i;if(a[i]<a[y])y=i;}
-        cout<<x<<" "<<y<<endl;
-    }return 0;
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    //freopen("D:\\VS-Code\\Competitive programming practice\\input.txt", "r", stdin);
+
+    long long n, q;
+    cin >> n >> q;
+    vector<long long> a(n);
+    for (long long i = 0; i < n; ++i) {
+        cin >> a[i];
+    }
+
+    long long N = 1;
+    while (N < n) {
+        N <<= 1;
+    }
+
+    vector<long long> tree(2 * N, 0);
+    for (long long i = 0; i < n; ++i) {
+        tree[N + i] = a[i];
+    }
+    for (long long i = N - 1; i >= 1; --i) {
+        tree[i] = tree[2 * i] + tree[2 * i + 1];
+    }
+
+    while (q--) {
+        long long type;
+        cin >> type;
+        if (type == 1) {
+            long long pos, val;
+            cin >> pos >> val;
+            long long idx = N + pos;
+            tree[idx] = val;
+            for (idx /= 2; idx >= 1; idx /= 2) {
+                tree[idx] = tree[2 * idx] + tree[2 * idx + 1];
+            }
+        } else {
+            long long l, r;
+            cin >> l >> r;
+            long long res = 0;
+            l += N;
+            r += N;
+            while (l < r) {
+                if (l % 2 == 1) {
+                    res += tree[l];
+                    l++;
+                }
+                if (r % 2 == 1) {
+                    r--;
+                    res += tree[r];
+                }
+                l /= 2;
+                r /= 2;
+            }
+            cout << res << '\n';
+        }
+    }
+
+    return 0;
 }
--- Zest-cases/vjudge.cpp
+++ Zest-cases/vjudge.cpp
@@ -1,77 +1,92 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#ifndef ONLINE_JUDGE
-#define LOCAL
-#endif
-
-#define ll long long
-#define f(x1, y1, z1) for (int x1 = y1; x1 < z1; x1++)
-#define endl "\n"
-#define yn(f) cout << (f ? "YES\n" : "NO\n")
-#define pr(x) cout << x << "\n"
-#define all(x) x.begin(), x.end()
-#define vl vector<ll>
-#define vi vector<int>
-#define pb push_back
-#define pi pair<ll, ll>
-
-#ifdef LOCAL
-#include "D:\\VS-Code\\Competitive programming practice\\Zest-cases\\dbg.h"
-#else
-#define dbg(x...)
-#endif
-
-const int MOD = 998244353;
-const int N = 1e5 + 5;
-
-void AKG(){
-	ll n, M;cin >> n >> M;
-
-	vector<ll> x(n), r(n);
-	f(i,0,n)cin >> x[i];
-	f(i,0,n)cin >> r[i];
-
-	ll mn = INT_MAX, mx = INT_MIN;
-	for(int i = 0; i < n; i++){
-		ll a  = x[i] - r[i];
-		ll b = x[i] + r[i];
-		if(a  < mn) mn = a;
-		if(b > mx) mx = b;
-	}
-
-	ll ans = 0;
-	for(ll X = mn; X <= mx; X++){
-		ll maxCross = -1;
-		for(int i = 0; i < n; i++){
-			ll distX = llabs(X - x[i]);
-			if(distX <= r[i]){
-				ll radius_sq = r[i] * r[i];
-				ll horizontal_sq = distX * distX;
-				ll vertical_sq = radius_sq - horizontal_sq;
-				ll cross = (ll) floorl( sqrtl((long double)vertical_sq) );
-				if(cross > maxCross) maxCross = cross;
-			}
-		}
-		if(maxCross >= 0) ans += (2LL * maxCross + 1LL);
-	}
-
-	pr(ans);
+int computeF(long long x, const vector<int>& centers, const vector<int>& radii) {
+    int best = -1;
+    int n = centers.size();
+    for (int i = 0; i < n; i++){
+        int d = abs((long long)x - centers[i]);
+        if(d <= radii[i]){
+            int cur = (int)floor(sqrt((long double) radii[i]*radii[i] - (long long)d*d));
+            best = max(best, cur);
+        }
+    }
+    return best;
 }
 
 int main(){
-#ifdef LOCAL
-  freopen("D:\\VS-Code\\Competitive programming practice\\input.txt", "r", stdin);
-#endif
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    // freopen("D:\\VS-Code\\Competitive programming practice\\input.txt", "r", stdin);
 
-  ios_base::sync_with_stdio(0);
-  cin.tie(0);
+    int t;
+    cin >> t;
+    while(t--){
+        int n, M;
+        cin >> n >> M;
 
-  int t = 1;
-  cin >> t;
-  while (t--){
-    AKG();
-  }
+        vector<int> centers(n), radii(n);
+        for (int i = 0; i < n; i++){
+            cin >> centers[i];
+        }
+        for (int i = 0; i < n; i++){
+            cin >> radii[i];
+        }
 
-  return 0;
+        int globalXmin = INT_MAX, globalXmax = INT_MIN;
+        for (int i = 0; i < n; i++){
+            globalXmin = min(globalXmin, centers[i] - radii[i]);
+            globalXmax = max(globalXmax, centers[i] + radii[i]);
+        }
+
+        vector<long long> cand;
+
+        cand.push_back(globalXmin);
+        cand.push_back((long long)globalXmax + 1);
+
+        for (int i = 0; i < n; i++){
+            int L_i = centers[i] - radii[i];
+            int R_i = centers[i] + radii[i];
+            cand.push_back(L_i);
+            cand.push_back((long long)R_i + 1);
+            for (int t_val = 0; t_val <= radii[i]; t_val++){
+                long double disc = (long double)radii[i]*radii[i] - (long double)t_val*t_val;
+                if(disc < 0) continue;
+                long double d = sqrt(disc);
+                long double leftPos = centers[i] - d;
+                long double rightPos = centers[i] + d;
+                long long c1 = (long long)floor(leftPos);
+                long long c2 = (long long)ceil(leftPos);
+                long long c3 = (long long)floor(rightPos);
+                long long c4 = (long long)ceil(rightPos);
+                c1 = max((long long)L_i, c1);
+                c2 = max((long long)L_i, c2);
+                c3 = min((long long)R_i, c3);
+                c4 = min((long long)R_i, c4);
+                cand.push_back(c1);
+                cand.push_back(c2);
+                cand.push_back(c3);
+                cand.push_back(c4);
+            }
+        }
+
+        sort(cand.begin(), cand.end());
+        cand.erase(unique(cand.begin(), cand.end()), cand.end());
+
+        long long ans = 0;
+        for (size_t i = 0; i + 1 < cand.size(); i++){
+            long long L = cand[i], R = cand[i+1];
+            if (R <= globalXmin || L > globalXmax) continue;
+            L = max((long long)globalXmin, L);
+            R = min((long long)globalXmax + 1, R);
+            if (L >= R) continue;
+            int f_val = computeF(L, centers, radii);
+            if(f_val < 0) continue;
+            long long countForInterval = (R - L) * (2LL * f_val + 1LL);
+            ans += countForInterval;
+        }
+
+        cout << ans << "\n";
+    }
+    return 0;
 }
