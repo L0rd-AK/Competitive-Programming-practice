93f08e94f210f90eaa72d10b3ff62c9e3f67457f
l0rd-AK <aka883207@gmail.com>
2025-03-12 23:22:22 +0600

    In the end

--- Zest-cases/ass.cpp
+++ Zest-cases/ass.cpp
@@ -1,63 +1,24 @@
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
+
+using ll = long long;
 
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
-    //freopen("D:\\VS-Code\\Competitive programming practice\\input.txt", "r", stdin);
-
-    long long n, q;
-    cin >> n >> q;
-    vector<long long> a(n);
-    for (long long i = 0; i < n; ++i) {
-        cin >> a[i];
+  freopen("D:\\VS-Code\\Competitive programming practice\\input.txt", "r", stdin);
+    
+    int T;
+    cin >> T;
+    while (T--) {
+        ll N;
+        cin >> N;
+        ll m = N / 2;
+        ll term1 = N * m * (m + 1) / 2;
+        ll term2 = -(m * (m + 1) * (2 * m + 1)) / 6;
+        ll term3 = m + 1;
+        ll sum = term1 + term2 + term3;
+        cout << sum << '\n';
     }
-
-    long long N = 1;
-    while (N < n) {
-        N <<= 1;
-    }
-
-    vector<long long> tree(2 * N, 0);
-    for (long long i = 0; i < n; ++i) {
-        tree[N + i] = a[i];
-    }
-    for (long long i = N - 1; i >= 1; --i) {
-        tree[i] = tree[2 * i] + tree[2 * i + 1];
-    }
-
-    while (q--) {
-        long long type;
-        cin >> type;
-        if (type == 1) {
-            long long pos, val;
-            cin >> pos >> val;
-            long long idx = N + pos;
-            tree[idx] = val;
-            for (idx /= 2; idx >= 1; idx /= 2) {
-                tree[idx] = tree[2 * idx] + tree[2 * idx + 1];
-            }
-        } else {
-            long long l, r;
-            cin >> l >> r;
-            long long res = 0;
-            l += N;
-            r += N;
-            while (l < r) {
-                if (l % 2 == 1) {
-                    res += tree[l];
-                    l++;
-                }
-                if (r % 2 == 1) {
-                    r--;
-                    res += tree[r];
-                }
-                l /= 2;
-                r /= 2;
-            }
-            cout << res << '\n';
-        }
-    }
-
     return 0;
 }
