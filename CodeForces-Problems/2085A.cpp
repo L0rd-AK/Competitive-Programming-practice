ce7ed0c467fe7482c7601df7ae8be48f3445a5d4
l0rd-AK <aka883207@gmail.com>
2025-03-24 21:45:45 +0600

    done with the flower

--- CodeForces-Problems/2085A.cpp
+++ CodeForces-Problems/2085A.cpp
@@ -26,41 +26,25 @@ const int MOD = 998244353;
 const int N = 2e5 + 5;
 
 void AKG() {
-    int n;cin>>n;
-    vl a(n),b;
+    int n,k;cin>>n>>k;
+    string s;cin>>s;
+    set<char> st;
     f(i,0,n){
-        cin>>a[i];
-        if(a[i]==0)b.push_back(i+1);
+        st.insert(s[i]);
     }
-    if(b.empty()){
-	 	cout<<"1\n";
-	 	cout<<1<<" "<<n<<"\n";
-	 	return ;
-	 }
-	 if(b.size()==n){
-	 	cout<<3<<endl;
-	 	cout<<"3 "<<n<<endl;
-	 	cout<<"1 2"<<endl;
-	 	cout<<"1 2"<<endl;
-	 	return ;
-	 }
-	 if(a[0]!=0){
-	 	cout<<2<<endl;
-	 	cout<<"2 "<<n<<endl;
-	 	cout<<"1 2"<<endl;
-	 	return ;
-	 }
-	 if(a[n-1]!=0){
-	 	cout<<2<<endl;
-	 	cout<<"1 "<<n-1<<endl;
-	 	cout<<"1 2"<<endl;
-	 	return ;
-	 }
-	 cout<<3<<endl;
-	 cout<<"3 "<<n<<endl;
-	 cout<<"1 2"<<endl;
-	 cout<<"1 2"<<endl;
-
+    f(i,0,n/2+1){
+        if(s[i]<s[n-i-1]){
+            yn(1);
+            return;
+        }
+        else if(s[i]==s[n-i-1])continue;
+        else if(s[i]>=s[n-i-1] && k>0 && st.size()>1){
+            yn(1);
+            return;
+        }else break;
+    }
+    if(k>0&&st.size()>1)yn(1);
+    else yn(0);
 }
 
 int main() {
