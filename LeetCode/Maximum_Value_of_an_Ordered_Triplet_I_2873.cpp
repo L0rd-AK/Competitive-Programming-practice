ee6ee637d4ad7a7d27fc78b067c95df19355c117
l0rd-AK <aka883207@gmail.com>
2025-04-02 21:43:14 +0600

    pain is the only true emotion

---
+++ LeetCode/Maximum_Value_of_an_Ordered_Triplet_I_2873.cpp
@@ -0,0 +1,21 @@
+#include <vector>
+#include <climits>
+
+class Solution {
+public:
+    long long maximumTripletValue(vector<int>& nums) {
+        int n = nums.size();
+        long long mx = LLONG_MIN;
+        
+        for (int i = 0; i < n; i++) {
+            for (int j = i+1; j < n; j++) {
+                for (int k = j+1; k < n; k++) {
+                    long long val = ((long long)nums[i] - (long long)nums[j]) * (long long) nums[k];
+                    mx = max(mx, val);
+                }
+            }
+        }
+        if(mx<0)return 0;
+        else return mx;
+    }
+};
