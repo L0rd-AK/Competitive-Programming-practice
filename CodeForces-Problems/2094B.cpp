90bd4f70a055ccbb575d4430e0ed93b232362d96
l0rd-AK <aka883207@gmail.com>
2025-04-13 23:33:06 +0600

    the less you know, the better

---
+++ CodeForces-Problems/2094B.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifndef ONLINE_JUDGE
+#define LOCAL
+#endif
+
+#define ll long long
+#define f(x1, y1, z1) for (int x1 = y1; x1 < z1; x1++)
+#define endl "\n"
+#define yn(f) cout << (f ? "YES\n" : "NO\n")
+#define pr(x) cout << x << "\n"
+#define all(x) x.begin(), x.end()
+#define vl vector<ll>
+#define vi vector<int>
+#define pb push_back
+#define pi pair<ll, ll>
+
+#ifdef LOCAL
+#include "D:\\VS-Code\\Competitive programming practice\\Zest-cases\\dbg.h"
+#else
+#define dbg(x...)
+#endif
+
+const int MOD = 1e9+7;
+const int N = 5e8+4;
+
+void AKG() {
+    int n,m,l,r;
+    cin >> n >> m >> l >> r;
+    int x = 0, y = 0;
+    while(1){ 
+        while(x > l && m > 0){
+            x--;
+            m--;
+        }
+        while(y < r && m > 0){
+            y++;
+            m--;
+        }
+        if(m == 0) break;
+    }
+    cout<<x<<" "<<y<<endl;
+}
+
+int main() {
+#ifdef LOCAL
+    freopen("D:\\VS-Code\\Competitive programming practice\\input.txt", "r", stdin);
+#endif
+
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        AKG();
+    }
+
+    return 0;
+}
