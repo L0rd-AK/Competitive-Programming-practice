d7bf6af03b632a22a6b3b9073b03b7d176519927
l0rd-AK <aka883207@gmail.com>
2025-04-14 23:48:37 +0600

    quota done

--- Zest-cases/test.cpp
+++ Zest-cases/test.cpp
@@ -25,42 +25,8 @@ using namespace std;
 const int MOD = 1e9+7;
 const int N = 5e8+4;
 
-vector<pair<char, int>> func(const string &x) {
-    vector<pair<char, int>> res;
-    if(x.empty()) return res;
-    int cnt = 1, n = x.size();
-    for (int i = 1; i < n; i++) {
-        if(x[i] == x[i-1])
-            cnt++;
-        else {
-            res.push_back({x[i-1], cnt});
-            cnt = 1;
-        }
-    }
-    res.push_back({x.back(), cnt});
-    return res;
-}
 void AKG() {
-    string a, b;
-    cin >> a >> b;
-    if(b.size() < a.size() || b.size() > 2 * a.size()){
-        yn(0);
-        return;
-    }
-    vector<pair<char,int>> v = func(a);
-    vector<pair<char,int>> w = func(b);
-    if(v.size() != w.size()){
-        yn(0);
-        return;
-    }
-    bool ok = true;
-    for(int i = 0; i < v.size(); i++){
-        if(v[i].first != w[i].first || w[i].second < v[i].second || w[i].second > 2 * v[i].second){
-            ok = false;
-            break;
-        }
-    }
-    yn(ok);
+    cout<<"security";
 }
 
 int main() {
@@ -72,7 +38,7 @@ int main() {
     cin.tie(0);
 
     int t = 1;
-    cin >> t;
+    // cin >> t;
     while (t--) {
         AKG();
     }
