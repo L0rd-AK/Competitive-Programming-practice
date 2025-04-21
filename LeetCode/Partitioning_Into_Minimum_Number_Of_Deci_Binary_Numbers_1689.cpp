038ae8af926bfb6ada1993474a9f9456c793d0be
l0rd-AK <aka883207@gmail.com>
2025-04-21 23:34:07 +0600

    can't stop wont stop

---
+++ LeetCode/Partitioning_Into_Minimum_Number_Of_Deci_Binary_Numbers_1689.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+    public:
+        int minPartitions(string n) {
+            int ans=0;
+            for(char c:n){
+                ans=max(ans,c-'0');
+            }
+            return ans;
+        }
+    };
