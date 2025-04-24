9f7142b827a56a72f6b38165963d1aa0579337f3
l0rd-AK <aka883207@gmail.com>
2025-04-24 23:46:56 +0600

    aded

---
+++ CodeForces-Problems/2106B.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifndef ONLINE_JUDGE
+#define LOCAL
+#endif
+
+#define ll long long
+#define f(x1, y1, z1) for (int x1 = y1; x1 < z1; x1++)
+#define endl "\n"
+#define yn(f) cout << (f ? "YES\n" : "NO\n")
+#define pr(x) cout << x << "\n"
+#define all(x) x.begin(), x.end()
+#define vl vector<ll>
+#define vi vector<int>
+#define pb push_back
+#define pi pair<ll, ll>
+
+#ifdef LOCAL
+#include "D:\\VS-Code\\Competitive programming practice\\Zest-cases\\dbg.h"
+#else
+#define dbg(x...)
+#endif
+
+const int MOD = 1e9+7;
+const int N = 5e8+4;
+
+void AKG() {
+    int n, x;cin >> n >> x;
+    vi a;
+    
+    if (x == 0) {
+        for (int i = 1; i < n; ++i)a.push_back(i);
+        a.push_back(0);
+    } else if (x == n) {
+        for (int i = 0; i < n; ++i)a.push_back(i);
+    } else {
+        for (int i = 0; i < x; ++i)a.push_back(i);
+
+        for (int i = x + 1; i < n; ++i)a.push_back(i);
+        a.push_back(x);
+    }
+    
+    for (int num : a)
+        cout << num << ' ';
+    cout << endl;
+}
+
+int main() {
+#ifdef LOCAL
+    freopen("D:\\VS-Code\\Competitive programming practice\\input.txt", "r", stdin);
+#endif
+
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        AKG();
+    }
+
+    return 0;
+}
