15971ff27966489bc08a3fa991f28659a3e1a02e
l0rd-AK <aka883207@gmail.com>
2025-05-19 00:13:05 +0600

    updated

--- LeetCode/How_many_numbers_are_smaller_than_the_current_number_1365.cpp
+++ LeetCode/How_many_numbers_are_smaller_than_the_current_number_1365.cpp
@@ -24,3 +24,24 @@ public:
         return ans;
     }
 };
+
+// 2nd approach
+class Solution {
+public:
+    vector<int> smallerNumbersThanCurrent(vector<int>& nums) {
+        map<int, int> m;
+        vector<int> vec = nums;
+        int n = nums.size();
+        sort(vec.begin(), vec.end());
+        
+        for(int i=n-1; i>=0; i--) {
+            m[vec[i]] = i;
+        }
+
+        for(int i=0; i<n; i++) {
+            nums[i] = m[nums[i]];
+        }
+        
+        return nums;
+    }
+};
