af96f9d293bb6d1b28021b05dd63cd47201f8d7a
l0rd-AK <aka883207@gmail.com>
2025-06-17 18:37:10 +0600

    codeforces done

--- Zest-cases/vjudge.cpp
+++ Zest-cases/vjudge.cpp
@@ -1,29 +1,35 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
-int main()
-{
-    long long a, n, sum, b, c;
-    cin >> a;
-    while (a--)
-    {
-        cin >> n;
-        sum = 0;
-        cin >> b;
-        n--;
-        while (n--)
-        {
-            cin >> c;
-            if (b * c > 0)
-            {
-                if (c > b)
-                    b = c;
+using ll = long long;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    freopen("D:\\VS-Code\\Competitive programming practice\\input.txt", "r", stdin);
+
+    int t;
+    cin >> t;
+    while (t--) {
+        ll n;
+        int k;
+        cin >> n >> k;
+        if (n >= k && (n % 2) == (k % 2)) {
+            cout << "YES\n";
+            for (int i = 0; i < k - 1; i++) {
+                cout << 1 << ' ';
             }
-            else
-            {
-                sum += b;
-                b = c;
+            cout << (n - (k - 1)) << '\n';
+        }else if (n >= 2LL * k && (n % 2) == 0) {
+            cout << "YES\n";
+          
+            for (int i = 0; i < k - 1; i++) {
+                cout << 2 << ' ';
             }
+            cout << (n - 2LL * (k - 1)) << '\n';
+        }else {
+            cout << "NO\n";
         }
-        cout << sum + b << endl;
     }
+
+    return 0;
 }
