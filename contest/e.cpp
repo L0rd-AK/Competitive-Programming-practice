9a45954f324bce96e3bfda8b89dbddad41a05d6e
l0rd-AK <aka883207@gmail.com>
2025-06-26 22:42:25 +0600

    codeforces done

--- contest/e.cpp
+++ contest/e.cpp
@@ -1,74 +1,27 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define ll long long int
-#define f(x1, y1, z1) for (int x1 = y1; x1 < z1; x1++)
-#define endl "\n"
-#define yn(f) f ? cout << "YES\n" : cout << "NO\n"
-#define prnt(x) cout << x << endl
-#define all(x) x.begin(), x.end()
 
-void __print(int x) {cerr << x;}
-void __print(long x) {cerr << x;}
-void __print(long long x) {cerr << x;}
-void __print(unsigned x) {cerr << x;}
-void __print(unsigned long x) {cerr << x;}
-void __print(unsigned long long x) {cerr << x;}
-void __print(float x) {cerr << x;}
-void __print(double x) {cerr << x;}
-void __print(long double x) {cerr << x;}
-void __print(char x) {cerr << '\'' << x << '\'';}
-void __print(const char *x) {cerr << '\"' << x << '\"';}
-void __print(const string &x) {cerr << '\"' << x << '\"';}
-void __print(bool x) {cerr << (x ? "true" : "false");}
-
-template<typename T, typename V>
-void __print(const pair<T, V> &x) {cerr << '{'; __print(x.first); cerr << ','; __print(x.second); cerr << '}';}
-template<typename T>
-void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i: x) cerr << (f++ ? "," : ""), __print(i); cerr << "}";}
-void _print() {cerr << "]\n";}
-template <typename T, typename... V>
-void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
-#ifndef ONLINE_JUDGE
-#define dbg(x...) cerr << "[" << #x << "] = ["; _print(x)
-#else
-#define dbg(x...)
-#endif
-
-ll gcd(ll a, ll b) {
-    while (b != 0) {
-        ll temp = b;
-        b = a % b;
-        a = temp;
-    }
-    return a;
-}
-
-ll lcm(ll a, ll b) {
-    return (a * b) / gcd(a, b);
-}
-
-void AKG() {
-    ll x, y;
-	cin >> x >> y;
-	    
-	ll a = ceil(x/(y+1.0));
-	ll b = x - 2*y;
-	prnt(max(a, b));
-}
-
-int main() {
-    #ifndef ONLINE_JUDGE
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
     freopen("D:\\VS-Code\\Competitive programming practice\\input.txt", "r", stdin);
-    #endif
 
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
-
-    int t = 1;
-    cin >> t;
-    while (t--) {
-        AKG();
+    int T;
+    cin >> T;
+    while (T--) {
+        int N;
+        cin >> N;
+        vector<int> A(N);
+        for (int i = 0; i < N; i++) {
+            cin >> A[i];
+        }
+        int result = INT_MAX;
+        for (int j = 0; j < N; j++) {
+            int d = abs(j - 0);
+            int s = max(d, A[j] + 1);
+            result = min(result, s);
+        }
+        cout << result << "\n";
     }
-
     return 0;
 }
