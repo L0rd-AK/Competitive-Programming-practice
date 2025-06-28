d3e6cb118ce75a90d6e7eccd49ccd32a5b0c9f04
l0rd-AK <aka883207@gmail.com>
2025-06-28 22:52:31 +0600

    working on

--- Zest-cases/test.cpp
+++ Zest-cases/test.cpp
@@ -8,52 +8,47 @@ using namespace std;
 #define ll long long
 #define f(x1, y1, z1) for (int x1 = y1; x1 < z1; x1++)
 #define endl "\n"
+#define yn(f) cout << (f ? "YES\n" : "NO\n")
 #define pr(x) cout << x << "\n"
+#define all(x) x.begin(), x.end()
 #define vl vector<ll>
+#define vi vector<int>
+#define pb push_back
+#define pi pair<ll, ll>
+
+#ifdef LOCAL
+#include "D:\\VS-Code\\Competitive programming practice\\Zest-cases\\dbg.h"
+#else
+#define dbg(x...)
+#endif
+
+const int MOD = 1e9+7;
+const int N = 5e8+4;
 
 void AKG() {
-    int n, k;
-    cin >> n >> k;
-    vl a(n);
-    ll sum = 0;
+    int n;
+    cin >> n;
+    map<int, int> mp;
     f(i, 0, n) {
-        cin >> a[i];
-        sum += a[i];
+        int x;
+        cin >> x;
+        mp[x]++;
     }
-    if (sum % k != 0) {
-        pr(n);
-        return;
-    }
-    int ans1 = 0, ans2 = 0;
-    int l = 0;
-    while (l < n) {
-        if ((sum - a[l]) % k != 0) {
-            ans1 = n - l - 1;
-            break;
-        }
-        l++;
-    }
-    int r = n - 1;
-    while (r >= 0) {
-        if ((sum - a[r]) % k != 0) {
-            ans2 = r;
-            break;
-        }
-        r--;
-    }
-    int ans = max(ans1, ans2);
-    if (ans == 0) pr(-1);
-    else pr(ans);
+
+    if (mp.size() == 1) yn(1);
+    else if (mp.size() == 2 && abs(begin(mp)->second - rbegin(mp)->second)<=1) yn(1);
+    else yn(0);
 }
 
 int main() {
 #ifdef LOCAL
     freopen("D:\\VS-Code\\Competitive programming practice\\input.txt", "r", stdin);
 #endif
+
     ios_base::sync_with_stdio(0);
     cin.tie(0);
-    int t;
-    cin >> t;
-    while (t--) AKG();
+
+    int t=1;cin >> t; 
+    while (t--)AKG();
     return 0;
 }
