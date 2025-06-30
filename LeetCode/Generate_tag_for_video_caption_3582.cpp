8dae85fadb7358d91231adc507706afe78667e59
l0rd-AK <aka883207@gmail.com>
2025-07-01 00:37:37 +0600

    leetcode done

--- LeetCode/Generate_tag_for_video_caption_3582.cpp
+++ LeetCode/Generate_tag_for_video_caption_3582.cpp
@@ -21,3 +21,22 @@ public:
         return ans;
     }
 };
+
+class Solution {
+public:
+    string generateTag(string caption) {
+        int n = caption.size();
+        string ans = "#";
+        for(int i = 0 ; i < n ; i++) {
+            if(caption[i] == ' ')continue;
+            if(i > 0 && caption[i - 1] == ' ') caption[i] = toupper(caption[i]);
+            else caption[i] = tolower(caption[i]);
+            ans += caption[i]; 
+        }
+
+        if(ans.size() >= 1) ans[1] =  tolower(ans[1]);
+        
+        return ans.substr(0,100);
+    
+    }
+};
