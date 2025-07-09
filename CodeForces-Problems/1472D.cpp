97cebeba7ae05299ab6b5c1524dcebd669bce31b
l0rd-AK <aka883207@gmail.com>
2025-07-09 22:36:43 +0600

    Add complete implementation of AKG function with input handling and game logic

---
+++ CodeForces-Problems/1472D.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifndef ONLINE_JUDGE
+#define LOCAL
+#endif
+
+#define ll long long
+#define f(x1, y1, z1) for (int x1 = y1; x1 < z1; x1++)
+#define endl "\n"
+#define yn(f) cout << (f ? "YES\n" : "NO\n")
+#define pr(x) cout << x << "\n"
+#define all(x) x.begin(), x.end()
+#define vl vector<ll>
+#define vi vector<int>
+#define pb push_back
+#define pi pair<ll, ll>
+
+#ifdef LOCAL
+#include "D:\\VS-Code\\Competitive programming practice\\Zest-cases\\dbg.h"
+#else
+#define dbg(x...)
+#endif
+
+const int MOD = 1e9+7;
+const int N = 5e8+4;
+
+void AKG() {
+    ll n;cin>>n;
+    vl a(n);
+    f(i,0,n)cin>>a[i];
+     sort(a.rbegin(), a.rend());
+    ll ans=0;
+    f(i,0,n){
+        if(i%2==0){if(a[i]%2==0)ans+=a[i];}
+        else if(a[i]%2==1) ans-=a[i];
+    }
+    if(ans==0)pr("Tie");
+    else if(ans>0)pr("Alice");
+    else pr("Bob");
+}
+
+int main() {
+#ifdef LOCAL
+    freopen("D:\\VS-Code\\Competitive programming practice\\input.txt", "r", stdin);
+#endif
+
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    int t=1;
+    cin >> t; 
+    while (t--)AKG();
+    return 0;
+}
