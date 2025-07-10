ac7578f1b9d44b6cc6b473e50b1ae5732d8862b6
l0rd-AK <aka883207@gmail.com>
2025-07-10 23:52:46 +0600

    Add initial implementation of AKG function with input handling and output logic

---
+++ CodeForces-Problems/2123C.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifndef ONLINE_JUDGE
+#define LOCAL
+#endif
+
+#define ll long long
+#define f(x1, y1, z1) for (int x1 = y1; x1 < z1; x1++)
+#define endl "\n"
+#define yn(f) cout << (f ? "YES\n" : "NO\n")
+#define pr(x) cout << x << "\n"
+#define all(x) x.begin(), x.end()
+#define vl vector<ll>
+#define vi vector<int>
+#define pb push_back
+#define pi pair<ll, ll>
+
+#ifdef LOCAL
+#include "D:\\VS-Code\\Competitive programming practice\\Zest-cases\\dbg.h"
+#else
+#define dbg(x...)
+#endif
+
+const int MOD = 1e9+7;
+const int N = 5e8+4;
+
+void AKG() {
+    ll n;cin >> n;
+    vl a(n);
+    f(i, 0, n) cin >> a[i];
+    ll mn = a[0], mx = a[n-1];
+    string ans(n,'0');
+    f(i,0,n){
+        mn=min(a[i],mn);
+        if(mn==a[i])ans[i]='1';
+    }
+    for(int i=n-1;i>=0;i--){
+        mx=max(mx,a[i]);
+        if(mx==a[i])ans[i]='1';
+    }
+    pr(ans);
+}
+
+int main() {
+#ifdef LOCAL
+    freopen("D:\\VS-Code\\Competitive programming practice\\input.txt", "r", stdin);
+#endif
+
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    int t=1;
+    cin >> t; 
+    while (t--)AKG();
+    return 0;
+}
