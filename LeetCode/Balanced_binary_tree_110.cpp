0bb5d13a6c2be622943ce05d4b382d930378d251
l0rd-AK <aka883207@gmail.com>
2025-07-11 22:50:02 +0600

    leetcode done

---
+++ LeetCode/Balanced_binary_tree_110.cpp
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int depth(TreeNode* root){
+        if(!root)return 0;
+        int left=depth(root->left);
+        int right=depth(root->right);
+        if(abs(left-right)>1)return -1e9;
+        return max(left,right)+1;
+    }
+    bool isBalanced(TreeNode* root) {
+        if(!root)return 1;
+        int left=depth(root->left);
+        if(left<0)return 0;
+        int right=depth(root->right);
+        if(right<0)return 0;
+        return abs(left-right)<=1;
+    }
+};
