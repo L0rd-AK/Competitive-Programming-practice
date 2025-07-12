f1ade9987efade92130121fb5885d6c40788a72f
l0rd-AK <aka883207@gmail.com>
2025-07-12 23:03:11 +0600

    not working

--- Zest-cases/test.cpp
+++ Zest-cases/test.cpp
@@ -28,8 +28,28 @@ const int N = 5e8+4;
 void AKG() {
     ll n,q;cin>>n>>q;
     string a,b;cin>>a>>b;
+    vi a1(26,0),b1(26,0);
+    f(i,0,n){
+        a1[a[i]-'a']++;
+        b1[b[i]-'a']++;
+    }
+    vector<vi> pre_a(n,vi(26,0)),pre_b(n,vi(26,0));
+    pre_a[0][0]=a1[0],pre_b[0][0]=b1[0];
+    f(i,1,n){
+        f(j,1,26){
+        pre_a[i][j]=a1[i]+pre_a[i-1][j];
+        pre_b[i][j]=b1[i]+pre_b[i-1][j];
+        }
+    }
     while(q--){
-        
+        int x,y;cin>>x>>y;
+        int ans=0;
+        for(char c='a';c<='z';c++){
+            int l=pre_a[y][c]-pre_a[x-1][c];
+            int r=pre_b[y][c]-pre_b[x-1][c];
+            ans+=(l-r);
+        }
+        pr(ans);
     }
     
 }
