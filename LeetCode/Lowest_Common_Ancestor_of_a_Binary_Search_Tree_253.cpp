87095d32a0b129c9312b831c0cb44ae0f1ac0964
l0rd-AK <aka883207@gmail.com>
2025-07-20 23:16:52 +0600

    workimg on leetcode

---
+++ LeetCode/Lowest_Common_Ancestor_of_a_Binary_Search_Tree_253.cpp
@@ -0,0 +1,20 @@
+  struct TreeNode {
+      int val;
+      TreeNode *left;
+      TreeNode *right;
+      TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+  };
+
+
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        TreeNode* tmp=root;
+        while(tmp!=nullptr){
+            if(p->val > tmp->val&&q->val > tmp->val)tmp=tmp->right;
+            else if(p->val < tmp->val&&q->val < tmp->val)tmp=tmp->left;
+            else return tmp;
+        }
+        return nullptr;
+    }
+};
