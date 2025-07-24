2bee1ab7336f58a536898186be758db6561e85e1
l0rd-AK <aka883207@gmail.com>
2025-07-24 22:43:23 +0600

    leetcode done

--- LeetCode/Numbers_of_1_bits.cpp
+++ LeetCode/Numbers_of_1_bits.cpp
@@ -4,3 +4,16 @@ public:
         return __builtin_popcount(n);
     }
 };
+
+class Solution {
+public:
+    int hammingWeight(uint32_t n) {
+        int res = 0;
+        for (int i = 0; i < 32; i++) {
+            if ((n >> i) & 1) {
+                res += 1;
+            }
+        }
+        return res;        
+    }
+};
