219e8dd57d6ceb1fd0a92046a9470c9d834f4a9d
l0rd-AK <aka883207@gmail.com>
2025-07-29 23:02:40 +0600

    code forces done

---
+++ CodeForces-Problems/2128A.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifndef ONLINE_JUDGE
+#define LOCAL
+#endif
+
+#define ll long long
+#define f(x1, y1, z1) for (int x1 = y1; x1 < z1; x1++)
+#define endl "\n"
+#define yn(f) cout << (f ? "YES\n" : "NO\n")
+#define pr(x) cout << x << "\n"
+#define all(x) x.begin(), x.end()
+#define vl vector<ll>
+#define vi vector<int>
+#define pb push_back
+#define pi pair<ll, ll>
+
+#ifdef LOCAL
+#include "D:\\VS-Code\\Competitive programming practice\\Zest-cases\\dbg.h"
+#else
+#define dbg(x...)
+#endif
+
+const int MOD = 1e9+7;
+const int N = 1e5+7;
+
+void AKG(){
+    ll n, c, cnt = 0, m = 1;
+    cin >> n >> c;
+    vl a(n);
+    f(i,0,n)cin >> a[i];
+    sort(a.rbegin(), a.rend());
+    for (auto to : a){
+        if (to * m <= c)m *= 2;
+        else cnt++;
+    }
+    pr(cnt);
+}
+
+int main() {
+#ifdef LOCAL
+    freopen("D:\\VS-Code\\Competitive programming practice\\input.txt", "r", stdin);
+#endif
+
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    int t=1;
+    cin >> t; 
+    while (t--)AKG();
+    return 0;
+}
