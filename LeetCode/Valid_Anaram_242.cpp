c06c67351ae4bd10cab1a763628b22a9974bb3a5
l0rd-AK <aka883207@gmail.com>
2025-07-31 22:22:31 +0600

    leetcode

--- LeetCode/Valid_Anaram_242.cpp
+++ LeetCode/Valid_Anaram_242.cpp
@@ -7,3 +7,19 @@ public:
         else return false;
     }
 };
+
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        int n=s.size(),m=t.size();
+        if(n!=m)return false;
+        int a[26]={0},b[26]={0};
+        for(int i=0;i<n;i++){
+            a[s[i]-'a']++;b[t[i]-'a']++;
+        }
+        for(int i=0;i<26;i++){
+            if(a[i]!=b[i])return false;
+        }
+        return true;
+    }
+};
