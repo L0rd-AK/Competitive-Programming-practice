eedb9545f1252bed419e95a026c9d8f9fb5ca7c8
l0rd-AK <aka883207@gmail.com>
2025-08-04 23:12:38 +0600

    updated

--- testy.cpp
+++ testy.cpp
@@ -1,12 +1,2 @@
-#include <bits/stdc++.h>
-using namespace std;
-int main()
-{
-    long long T, n, k, x;
-    cin >> T;
-    while (T--)
-    {
-        cin >> n >> k >> x;
-        cout << ((k * (k + 1) / 2 > x || (n - k + 1 + n) * k / 2 < x) ? "NO\n" : "YES\n");
-    }
-}
+#include<bits/stdc++.h>
+using namespace std;int main(){long long T,n,k,x;cin>>T;while(T--){cin>>n>>k>>x;}}
