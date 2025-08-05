a585b767b08a05c86edd7a74c5864bf2da8a0bc3
l0rd-AK <aka883207@gmail.com>
2025-08-05 23:37:14 +0600

    practicing

--- test.cpp
+++ test.cpp
@@ -26,8 +26,33 @@ const int MOD = 1e9+7;
 const int N = 1e5+7;
 
 void AKG(){
-    ll n,k,x;cin>>n>>k>>x;
-    cout<<((k*(k+1)/2>x||(n-k+1+n)*k/2<x)?"NO\n":"YES\n");
+    int n;
+    cin >> n;
+    vl b(n);
+
+    ll min_b = 1e9 + 1;
+    ll max_b = 0;
+
+    for (int i = 0; i < n; ++i) {
+        cin >> b[i];
+        if (b[i] < min_b) min_b = b[i];
+        if (b[i] > max_b) max_b = b[i];
+    }
+
+    ll count_min = 0, count_max = 0;
+    for (int i = 0; i < n; ++i) {
+        if (b[i] == min_b) ++count_min;
+        if (b[i] == max_b) ++count_max;
+    }
+
+    if (min_b == max_b) {
+        ll ways = (ll)n * (n - 1) / 2;
+        cout << "0 " << ways << endl;
+    } else {
+        ll max_diff = max_b - min_b;
+        ll ways = count_min * count_max;
+        cout << max_diff << " " << ways << endl;
+    }
 }
 
 int main() {
@@ -39,7 +64,7 @@ int main() {
     cin.tie(0);
 
     int t=1;
-    cin >> t; 
+    // cin >> t; 
     while (t--)AKG();
     return 0;
 }
