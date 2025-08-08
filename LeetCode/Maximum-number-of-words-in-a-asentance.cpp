dd5c264ee1b33361c2463e1c1c14712f97d9ad4f
l0rd-AK <aka883207@gmail.com>
2025-08-08 22:52:45 +0600

    leetcode done

--- LeetCode/Maximum-number-of-words-in-a-asentance.cpp
+++ LeetCode/Maximum-number-of-words-in-a-asentance.cpp
@@ -13,3 +13,12 @@ class Solution {
             return ans+1;
         }
     };
+
+
+    class Solution {
+public:
+    int mostWordsFound(vector<string>& s) {
+    return 1 + accumulate(begin(s), end(s), 0, [](int res, const auto &s) {
+        return max(res, (int)count(begin(s), end(s), ' ')); });
+}
+};
