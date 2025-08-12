e2a302da64727c0c4d17e51a8cc3cb0296f3f925
l0rd-AK <aka883207@gmail.com>
2025-08-12 23:20:53 +0600

    leetcode done

--- LeetCode/Find_the_duplicate_number_287.cpp
+++ LeetCode/Find_the_duplicate_number_287.cpp
@@ -35,3 +35,27 @@ public:
         return -1; 
     }
 };
+
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        // Floyd's Tortoise and Hare
+        int tortoise = nums[0];
+        int hare = nums[0];
+
+        // 1) Find intersection point
+        do {
+            tortoise = nums[tortoise];
+            hare = nums[nums[hare]];
+        } while (tortoise != hare);
+
+        // 2) Find entrance to the cycle (the duplicate)
+        tortoise = nums[0];
+        while (tortoise != hare) {
+            tortoise = nums[tortoise];
+            hare = nums[hare];
+        }
+
+        return tortoise; // the duplicate value
+    }
+};
