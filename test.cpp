147b41c330b11beb5fe09fe3a9f97be2ff711390
l0rd-AK <aka883207@gmail.com>
2025-08-15 23:33:48 +0600

    Add array initialization in AKG function and update input handling in testy.cpp

--- test.cpp
+++ test.cpp
@@ -24,7 +24,7 @@ using namespace std;
 
 const int MOD = 1e9+7;
 const int N = 1e5+7;
-
+int n[32] = {3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5, 8, 9, 7, 9, 3, 2, 3, 8, 4, 6, 2, 6, 4, 3, 3, 8, 3, 2, 7, 9, 5};
 void AKG(){
   
 }
--- testy.cpp
+++ testy.cpp
@@ -1,5 +1,10 @@
-#include <iostream>
+#include "cstdio"
+int t, ans, x;
 main()
 {
-    
+    scanf("%d", &t);
+    for (int i = 1; i <= t; i++)
+    {
+        
+    }
 }
