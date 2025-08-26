30bf61b184d92c2a326dd106d13ce7de1b77ea36
l0rd-AK <aka883207@gmail.com>
2025-08-26 23:27:41 +0600

    Implement Binary Search Tree to Greater Sum Tree conversion

---
+++ LeetCode/Binary_Search_Tree_to_Greater_Sum_Tree_1038.cpp
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int sum=0;
+    void travers(TreeNode* root) {
+        if(root){
+            travers(root->right);
+            sum+=root->val;
+            root->val=sum;
+            travers(root->left);
+        }
+    }
+    TreeNode* bstToGst(TreeNode* root) {
+        travers(root);
+        return root;
+    }
+};
