b4d334936a3b29a878ff827ec4dcd0d8a27e2140
l0rd-AK <aka883207@gmail.com>
2025-08-27 22:23:24 +0600

    Initial commit of 115A.cpp with AKG function implementation and input handling

---
+++ CodeForces-Problems/115A.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifndef ONLINE_JUDGE
+#define LOCAL
+#endif
+
+#define ll long long
+#define f(x1, y1, z1) for (int x1 = y1; x1 < z1; x1++)
+#define endl "\n"
+#define yn(f) cout << (f ? "YES\n" : "NO\n")
+#define pr(x) cout << x << "\n"
+#define all(x) x.begin(), x.end()
+#define vl vector<ll>
+#define vi vector<int>
+#define pb push_back
+#define pi pair<ll, ll>
+
+#ifdef LOCAL
+#include "D:\\VS-Code\\Competitive programming practice\\Zest-cases\\dbg.h"
+#else
+#define dbg(x...)
+#endif
+
+const int MOD = 1e9+7;
+ll N = 1e18;
+
+
+void AKG() {
+ int n, m = 0, k, i = 1, c, a[2002];
+    for(cin>>n;i<=n;)cin>>a[i++];
+    for(i=1;i<=n;){
+        k=a[i++],c=0;
+        while(k>0)
+            k=a[k],c++;
+        m=c>m?c:m;
+    }
+    pr(m+1);
+}
+
+
+int main() {
+#ifdef LOCAL
+    freopen("D:\\VS-Code\\Competitive programming practice\\input.txt", "r", stdin);
+#endif
+
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    int t=1;
+    // cin >> t; 
+    while (t--)AKG();
+    return 0;
+}
