93b296152b28908b31cd37832e31c6f52793a4c3
l0rd-AK <aka883207@gmail.com>
2025-08-27 22:22:51 +0600

    Update input data and refactor AKG function for improved logic and efficiency

--- test.cpp
+++ test.cpp
@@ -27,19 +27,15 @@ ll N = 1e18;
 
 
 void AKG() {
-  int n;cin>>n;
-  set<int> st;
-  bool ok=0;
-  vi a(n);
-  f(i,0,n)cin>>a[i];
-  f(i,0,n){
-    if(st.count(a[i])){
-        yn(1);
-        return;
+ int n, m = 0, k, i = 1, c, a[2002];
+    for(cin>>n;i<=n;)cin>>a[i++];
+    for(i=1;i<=n;){
+        k=a[i++],c=0;
+        while(k>0)
+            k=a[k],c++;
+        m=c>m?c:m;
     }
-    st.insert(a[i]);
-  }
-  yn(0);
+    pr(m+1);
 }
 
 
@@ -52,7 +48,7 @@ int main() {
     cin.tie(0);
 
     int t=1;
-    cin >> t; 
+    // cin >> t; 
     while (t--)AKG();
     return 0;
 }
--- testy.cpp
+++ testy.cpp
@@ -1,25 +1,16 @@
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
-
-int main() {
-    ios::sync_with_stdio(false); cin.tie(nullptr);
-    freopen("D:\\VS-Code\\Competitive programming practice\\input.txt", "r", stdin);
-
-    int n; 
-    if(!(cin >> n)) return 0;
-    vector<long long> a(n);
-    long long sum = 0;
-    for (auto &x : a) cin >> x, sum += x;
-
-    if (sum & 1LL) { cout << 0 << '\n'; return 0; }
-
-    long long target = sum / 2, pref = 0;
-    long long ans = 0;
-    // split must be between [0 .. n-2] so both parts are non-empty
-    for (int i = 0; i < n - 1; ++i) {
-        pref += a[i];
-        if (pref == target) ++ans;
+int main()
+{
+    int n, m = 0, k, i = 1, c, a[2002];
+    for (cin >> n; i <= n;)
+        cin >> a[i++];
+    for (i = 1; i <= n;)
+    {
+        k = a[i++], c = 0;
+        while (k > 0)
+            k = a[k], c++;
+        m = c > m ? c : m;
     }
-    cout << ans << '\n';
-    return 0;
+    cout << m + 1;
 }
