28d59ec1cd6537936ae1815910736a5daa455859
l0rd-AK <aka883207@gmail.com>
2025-09-02 23:47:50 +0600

    leetcode done

--- LeetCode/Binary_tree_maximum_path_sum_124.cpp
+++ LeetCode/Binary_tree_maximum_path_sum_124.cpp
@@ -27,3 +27,50 @@ public:
         return ans;
     }
 };
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+
+    int maxDepth(TreeNode* root, int& ans) {
+
+        if(root == NULL){
+            return 0;
+        }   
+
+        int leftt = maxDepth(root->left, ans);
+        int rightt = maxDepth(root->right, ans);
+
+        if(leftt < 0){
+            leftt = 0;
+        }
+        if(rightt < 0){
+            rightt = 0;
+        }
+
+        ans = max(ans, leftt + rightt + root->val);
+
+        return max(leftt + root->val, rightt + root->val);
+    }
+
+    int maxPathSum(TreeNode* root) {
+
+        int ans = INT_MIN;
+        maxDepth(root, ans);
+        root->left = nullptr;
+        root->right = nullptr;
+        root = nullptr;
+        return ans;
+        
+    }
+};
