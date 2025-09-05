fd37c42a6ebf966e4845dfa7708e418e5a4ab556
l0rd-AK <aka883207@gmail.com>
2025-09-05 23:35:13 +0600

    leetcode done

--- LeetCode/Find_Closest_person_3516.cpp
+++ LeetCode/Find_Closest_person_3516.cpp
@@ -9,3 +9,10 @@ public:
         else return 2;
     }
 };
+
+class Solution {
+public:
+    int findClosest(int x, int y, int z) {
+        return abs(x-z)==abs(y-z)?0:2-(abs(x-z)<abs(y-z));
+    }
+};
