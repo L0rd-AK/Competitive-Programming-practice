9ba20f4c370403eea11aaac5516531a1e07e206d
l0rd-AK <aka883207@gmail.com>
2025-09-13 23:55:15 +0600

    Remove unused variables and logic from AKG function

--- test.cpp
+++ test.cpp
@@ -26,14 +26,7 @@ const int MOD = 1e9+7;
 ll N = 1e18;
 
 void AKG(){
-    int n;cin>>n;
-    vi a(n);
-    ll mn=N;
-    f(i,0,n){
-        cin>>a[i];
-        mn=(mn<a[i]?mn:a[i]);
-    }
-    pr((mn^a[2])+2);
+    
 }
 
 int main() {
