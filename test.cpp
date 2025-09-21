f13a57b76090034c9b379e98a65aed8b2368084c
l0rd-AK <aka883207@gmail.com>
2025-09-21 23:53:15 +0600

    Improve formatting of AKG function for better readability

--- test.cpp
+++ test.cpp
@@ -30,12 +30,12 @@ void AKG(){
     cin>>n>>m;
     for(int i=0;i<n;++i)
     {
-    int a,b;
-    cin>>a>>b;
-    if(a%2!=(b+in)%2){
-        --m;
-        in=in^1;
-    }
+        int a,b;
+        cin>>a>>b;
+        if(a%2!=(b+in)%2){
+            --m;
+            in=in^1;
+        }
     }
     cout<<m<<endl;
 }
