ba9ef986d7a345594ef60bb0d8fdeb5570d7d51d
l0rd-AK <aka883207@gmail.com>
2025-09-22 23:48:04 +0600

    Format code for better readability and maintainability

--- CodeForces-Problems/748G.cpp
+++ CodeForces-Problems/748G.cpp
@@ -1,2 +1,15 @@
-#include<iostream>
-int a,b,j;char c;using namespace std;main(){for(cin>>a;cin>>c;)cin>>b,c=='+'?a+=b:a-=b;for(auto i:to_string(a)){for(j=0;j<i;j++)cout<<"+";cout<<".>\n";}}
+#include <iostream>
+int a, b, j;
+char c;
+using namespace std;
+main()
+{
+    for (cin >> a; cin >> c;)
+        cin >> b, c == '+' ? a += b : a -= b;
+    for (auto i : to_string(a))
+    {
+        for (j = 0; j < i; j++)
+            cout << "+";
+        cout << ".>\n";
+    }
+}
