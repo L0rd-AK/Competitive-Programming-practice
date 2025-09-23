dc0077bb20fb432be63cd5bbd228ec5e2b6d1dd9
l0rd-AK <aka883207@gmail.com>
2025-09-23 22:31:27 +0600

    Add input redirection for reading test cases from file

--- testy.cpp
+++ testy.cpp
@@ -4,6 +4,7 @@ using namespace std;
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
+    freopen("D:\\VS-Code\\Competitive programming practice\\input.txt", "r", stdin);
 
     int t;
     cin >> t;
