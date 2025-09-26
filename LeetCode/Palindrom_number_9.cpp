456b1c0dbb1f9226a22d29b226b434c2f261c17f
l0rd-AK <aka883207@gmail.com>
2025-09-26 23:16:57 +0600

    leetcode done

--- LeetCode/Palindrom_number_9.cpp
+++ LeetCode/Palindrom_number_9.cpp
@@ -9,3 +9,21 @@ class Solution {
             return a==s;
         }
     };
+
+    class Solution {
+public:
+    bool isPalindrome(int x) {
+        // Negative numbers and numbers ending with 0 (but not 0 itself) are not palindromes
+        if (x < 0 || (x % 10 == 0 && x != 0)) return false;
+
+        int rev = 0;
+        while (x > rev) {
+            rev = rev * 10 + x % 10;
+            x /= 10;
+        }
+
+        // If the number of digits is even: x == rev
+        // If odd: x == rev/10 (middle digit ignored)
+        return x == rev || x == rev / 10;
+    }
+};
