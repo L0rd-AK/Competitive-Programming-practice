762618720d78e0d2e661dc20b3c38eb9b18eca74
l0rd-AK <aka883207@gmail.com>
2025-09-28 22:54:11 +0600

    Refactor AKG function to calculate sum and handle output based on its divisibility by 3

--- test.cpp
+++ test.cpp
@@ -28,7 +28,10 @@ ll N = 1e18;
 void AKG(){
    int n;cin>>n;
    vi a(n);
-   f(i,0,n)cin>>a[i];
+   ll sum=0;
+   f(i,0,n)cin>>a[i],sum+=a[i];
+   if(sum%3)pr("0 0");
+   else cout<<1<<" "<<n-1<<endl;
 }
 
 int main() {
