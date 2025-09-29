c5477dd4b18e05c6360532c1a60b90d012340bc2
l0rd-AK <aka883207@gmail.com>
2025-09-29 23:44:15 +0600

    Refactor main function structure and remove unused code for clarity

--- testy.cpp
+++ testy.cpp
@@ -1,44 +1,11 @@
 #include <bits/stdc++.h>
 using namespace std;
-
-int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-    freopen("D:\\VS-Code\\Competitive programming practice\\input.txt", "r", stdin);
-
+int main()
+{
     int t;
     cin >> t;
-    while (t--) {
-        int n;
-        cin >> n;
-        vector<int> a(n);
-        for (int i = 0; i < n; i++) {
-            cin >> a[i];
-        }
-        // Since a is non-decreasing and values are in [1..n], we can count directly.
-        vector<int> cnt(n + 1, 0);
-        for (int x : a) {
-            cnt[x]++;
-        }
-        // Collect non-zero counts.
-        vector<int> freqs;
-        for (int v = 1; v <= n; v++) {
-            if (cnt[v] > 0) {
-                freqs.push_back(cnt[v]);
-            }
-        }
-        int ans = 0;
-        // Try every possible k from 1 up to n.
-        for (int k = 1; k <= n; k++) {
-            int c = 0;
-            for (int f : freqs) {
-                if (f >= k) {
-                    c++;
-                }
-            }
-            ans = max(ans, c * k);
-        }
-        cout << ans << "\n";
+    while (t--)
+    {
+        
     }
-    return 0;
 }
