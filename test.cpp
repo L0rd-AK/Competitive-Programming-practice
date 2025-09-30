4a566a995276ef5a0e7d443c6a447f25ea4d7b73
l0rd-AK <aka883207@gmail.com>
2025-09-30 22:42:35 +0600

    Refactor AKG function to improve character counting and streamline logic

--- test.cpp
+++ test.cpp
@@ -26,32 +26,24 @@ const int MOD = 1e9+7;
 ll N = 1e18;
 
 void AKG(){
-    string s; 
-    cin >> s;
-    int cnt[4] = {0,0,0,0};
-    for(char c : s)cnt[c - '0']++;
+    string s;cin >> s;
+    int n = (int)s.size();
+    vi cnt(4, 0);
+    int l = 0;
+    int ans = INT_MAX;
 
-    int l = 0, r = (int)s.length() - 1;
-    while (l <= r) {
-        bool moved = false;
+    for (int r = 0; r < n; ++r) {
+        ++cnt[s[r] - '0'];
 
-        if (l <= r) {
-            if (s[l] == '1' && cnt[1] > 1) { cnt[1]--; l++; moved = true; }
-            else if (s[l] == '2' && cnt[2] > 1) { cnt[2]--; l++; moved = true; }
-            else if (s[l] == '3' && cnt[3] > 1) { cnt[3]--; l++; moved = true; }
+        while (cnt[1] > 0 && cnt[2] > 0 && cnt[3] > 0 && l <= r) {
+            ans = min(ans, r - l + 1);
+            --cnt[s[l] - '0'];
+            ++l;
         }
-
-        if (l <= r) {
-            if (s[r] == '1' && cnt[1] > 1) { cnt[1]--; r--; moved = true; }
-            else if (s[r] == '2' && cnt[2] > 1) { cnt[2]--; r--; moved = true; }
-            else if (s[r] == '3' && cnt[3] > 1) { cnt[3]--; r--; moved = true; }
-        }
-
-        if (!moved) break;
     }
-    int x=r - l + 1;
-    if(x<3)pr(0);
-    else pr(x);
+
+    if (ans == INT_MAX) cout << 0 << '\n';
+    else cout << ans << '\n';
 }
 
 int main() {
