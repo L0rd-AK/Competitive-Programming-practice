b0d113a54a9a1587cd07fe4671c03b48b61ca864
l0rd-AK <aka883207@gmail.com>
2025-10-04 22:05:16 +0600

    Implement AKG function to read input and output corresponding array value

--- test.cpp
+++ test.cpp
@@ -26,7 +26,9 @@ const int MOD = 1e9+7;
 ll N = 1e18;
 
 void AKG(){
-    
+    int a[] = {0, 15, 14, 12, 13, 8, 9, 10, 11, 0, 1, 2, 3, 4, 5, 6, 7};
+    std::cin >> a[0];
+    std::cout << a[a[0] + 1];
 }
 
 int main() {
