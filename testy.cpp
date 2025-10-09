e19b992d37e8915bb0d50dfcc93979552934830d
l0rd-AK <aka883207@gmail.com>
2025-10-09 23:36:05 +0600

    Remove unused input handling and logic from main function in testy.cpp

--- testy.cpp
+++ testy.cpp
@@ -9,16 +9,7 @@ int main() {
     int t;
     cin >> t;
     while (t--) {
-        int n;
-        cin >> n;
-        vector<int> a(n);
-        for (int i = 0; i < n; i++) {
-            cin >> a[i];
-        }
-        unordered_set<int> uniq(a.begin(), a.end());
-        int D = uniq.size();
-        int x= 2 * D - 1;
-        cout << x << "\n";
+        
     }
     return 0;
 }
