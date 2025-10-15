88443494b6bf00b1ae57b61464b062441cf4f62b
unknown <aka883207@gmail.com>
2025-10-15 22:46:21 +0600

    codforce done

---
+++ CodeForces-Problems/1742E.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifndef ONLINE_JUDGE
+#define LOCAL
+#endif
+
+#define ll long long
+#define f(x1, y1, z1) for (int x1 = y1; x1 < z1; x1++)
+#define endl "\n"
+#define yn(f) cout << (f ? "YES\n" : "NO\n")
+#define pr(x) cout << x << "\n"
+#define all(x) x.begin(), x.end()
+#define vl vector<ll>
+#define vi vector<int>
+#define pb push_back
+#define pi pair<ll, ll>
+
+ll a[200005];
+ll b[200005];
+
+const int MOD = 1e9+7;
+ll N = 1e18;
+
+void AKG(){
+    ll n,q;cin >> n >> q;
+    //ll a[n+1],b[n+1];
+    for (int i = 1; i <= n; ++i){
+          ll x;cin >> x;
+          a[i] = max(a[i - 1], x), b[i] = b[i - 1] + x;
+    }
+            
+    while (q--){
+        ll x;cin >> x;
+        cout << b[upper_bound(a + 1, a + n + 1, x) - a - 1] << ' ';
+    }
+    cout << '\n';
+}
+
+int main() {
+
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    int t=1;
+    cin >> t; 
+    while (t--)AKG();
+    return 0;
+}
