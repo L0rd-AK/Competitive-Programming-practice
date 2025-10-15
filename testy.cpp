4bc7048c8b0a8ad55de0249d22398e3800086e80
unknown <aka883207@gmail.com>
2025-10-15 22:28:40 +0600

    working on code

--- testy.cpp
+++ testy.cpp
@@ -1,15 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
+int a[200005];
+long long b[200005];
+int main()
+{
+    int T, i, x, n, q;
 
-int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-    freopen("D:\\VS-Code\\Competitive programming practice\\input.txt", "r", stdin);
-
-    int t;
-    cin >> t;
-    while (t--) {
-        
+    for (cin >> T; T--;)
+    {
+        cin >> n >> q;
+        for (i = 1; i <= n; ++i)
+            cin >> x, a[i] = max(a[i - 1], x), b[i] = b[i - 1] + x;
+        while (q--)
+        {
+            cin >> x;
+            cout << b[upper_bound(a + 1, a + n + 1, x) - a - 1] << ' ';
+        }
+        cout << '\n';
     }
+
     return 0;
 }
