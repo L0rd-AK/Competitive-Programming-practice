cce9a32f5641e3b1c80625f417c5c85df62379e5
unknown <aka883207@gmail.com>
2025-10-16 23:06:56 +0600

    working on codeforces

--- testy.cpp
+++ testy.cpp
@@ -8,15 +8,7 @@ int main()
 
     for (cin >> T; T--;)
     {
-        cin >> n >> q;
-        for (i = 1; i <= n; ++i)
-            cin >> x, a[i] = max(a[i - 1], x), b[i] = b[i - 1] + x;
-        while (q--)
-        {
-            cin >> x;
-            cout << b[upper_bound(a + 1, a + n + 1, x) - a - 1] << ' ';
-        }
-        cout << '\n';
+        
     }
 
     return 0;
