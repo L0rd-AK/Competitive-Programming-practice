7e6ffe6c3c1a9d50980cf5bdba13cae72a3682b8
l0rd-AK <aka883207@gmail.com>
2025-10-20 21:11:49 +0600

    Remove unused vector and output logic from AKG function

--- test.cpp
+++ test.cpp
@@ -28,11 +28,7 @@ ll N = 1e18;
 void AKG(){
     int n;cin>>n;
     string s;cin>>s;
-    vi a;
-    f(i,0,n)if(s[i]=='1')a.pb(i+1);
-    pr(a.size());
-    for(int i:a)cout<<i<<" ";
-    cout<<endl;
+    
 }
 
 int main() {
