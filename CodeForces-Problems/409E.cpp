60eee52ab9a5973ecf4b007808e68695f685bf31
l0rd-AK <aka883207@gmail.com>
2025-10-22 23:31:42 +0600

    codeforces done

---
+++ CodeForces-Problems/409E.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifndef ONLINE_JUDGE
+#define LOCAL
+#endif
+
+#define ll long long
+#define f(x1, y1, z1) for (int x1 = y1; x1 < z1; x1++)
+#define endl "\n"
+#define yn(f) cout << (f ? "YES\n" : "NO\n")
+#define pr(x) cout << x << "\n"
+#define all(x) x.begin(), x.end()
+#define vl vector<ll>
+#define vi vector<int>
+#define pb push_back
+#define pi pair<ll, ll>
+
+#ifdef LOCAL
+#include "D:\\VS-Code\\Competitive programming practice\\Zest-cases\\dbg.h"
+#else
+#define dbg(x...)
+#endif
+
+const int MOD = 1e9+7;
+ll N = 1e18;
+
+void AKG(){
+    double x;cin>>x;
+    for (int i=1;i<=10;++i) 
+        for (int j=1;j<=10;++j) 
+            if (fabs(i*j/sqrt(i*i+4*j*j)-x)<=1e-5){
+                cout<<i<<" "<<j<<endl;
+                return;
+            }
+}
+
+int main() {
+#ifdef LOCAL
+    freopen("D:\\VS-Code\\Competitive programming practice\\input.txt", "r", stdin);
+#endif
+
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    int t=1;
+    // cin >> t; 
+    while (t--)AKG();
+    return 0;
+}
