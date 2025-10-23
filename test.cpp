33f63fc96f353e01ab9cb5ea70bc2ec5bf2c366b
l0rd-AK <aka883207@gmail.com>
2025-10-23 21:40:11 +0600

    Remove input handling and nested loops from AKG function for simplification

--- test.cpp
+++ test.cpp
@@ -26,13 +26,7 @@ const int MOD = 1e9+7;
 ll N = 1e18;
 
 void AKG(){
-    double x;cin>>x;
-    for (int i=1;i<=10;++i) 
-        for (int j=1;j<=10;++j) 
-            if (fabs(i*j/sqrt(i*i+4*j*j)-x)<=1e-5){
-                cout<<i<<" "<<j<<endl;
-                return;
-            }
+    
 }
 
 int main() {
