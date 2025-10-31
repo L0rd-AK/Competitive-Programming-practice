c6ba1d5c97688877eafa79df6ab39d24a7f4563f
l0rd-AK <aka883207@gmail.com>
2025-10-31 23:27:47 +0600

    Refactor AKG function to improve logic for handling input cases and update result string generation

--- test.cpp
+++ test.cpp
@@ -8,7 +8,6 @@ using namespace std;
 #define ll long long
 #define f(x1, y1, z1) for (int x1 = y1; x1 < z1; x1++)
 #define endl "\n"
-// #define yn(f) cout << (f ? "YES\n" : "NO\n")
 #define yn(f) cout << (f ? "BOB\n" : "ALICE\n")
 #define pr(x) cout << x << "\n"
 #define all(x) x.begin(), x.end()
@@ -29,22 +28,36 @@ ll N = 1e18;
 void AKG(){
   int n,k;cin>>n>>k;
   string s;cin>>s;
-  string ans="";
-  f(i,0,n)ans+='+';
-  int l=0,r=n-1;
+  int min_top = 0, max_top = 0;
+  int min_bot = 0, max_bot = 0;
+  
   f(i,0,k){
     if(s[i]=='0'){
-      ans[l]='-';l++;
+      min_top++;
+      max_top++;
     }else if(s[i]=='1'){
-      ans[r]='-'; r++;
-    }else{
-      if(i==r){ans[i]='-';}
-      else{
-        ans[l]='?',ans[r]='?';
-        l++,r--;
-      }
+      min_bot++;
+      max_bot++;
+    }else{ 
+      max_top++;
+      max_bot++;
     }
   }
+  
+  string ans="";
+  f(i,0,n){
+    bool definitely_removed = (i < min_top) || (i >= n - min_bot);
+    bool definitely_safe = (i >= max_top) && (i < n - max_bot);
+    
+    if(definitely_removed){
+      ans += '-';
+    }else if(definitely_safe){
+      ans += '+';
+    }else{
+      ans += '?';
+    }
+  }
+  
   pr(ans);
 }
 
