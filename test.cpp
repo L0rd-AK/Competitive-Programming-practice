03b0c4f5c9aa9eacb725f1608d7e9f150755fa75
l0rd-AK <aka883207@gmail.com>
2025-11-03 21:27:56 +0600

    Remove input handling and logic from AKG function for future refactoring

--- test.cpp
+++ test.cpp
@@ -26,20 +26,7 @@ const int MOD = 1e9+7;
 ll N = 1e18;
 
 void AKG(){
-  int n;cin>>n;
-  set<int> st;
-  f(i,0,n){
-    int x;cin>>x;
-    st.insert(x);
-  }
-   vi a(all(st));
-  f(i,0,n){
-    if(i!=a[i]){
-      pr(i);
-      return;
-    }
-  }
-  pr(a[n-1]+1);
+  
 }
 
 int main() {
