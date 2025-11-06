f1a727b34f5f2afe1b15f4aa38436f3e8f5e0b7c
l0rd-AK <aka883207@gmail.com>
2025-11-06 23:39:00 +0600

    Refactor variable declarations and streamline input handling in main function

--- testy.cpp
+++ testy.cpp
@@ -4,65 +4,44 @@ int main()
 {
     ios::sync_with_stdio(false);
     cin.tie(0);
-    long long tt;
+    ll tt;
     cin>>tt;
     while(tt--)
     {
-         long long n,q;
-         cin>>n>>q;
-         long long mx=600;
-         char arr[620];
-         string s;
-         cin>>s;
+         ll n,q;cin>>n>>q;
+         ll mx=600;
+         string s;cin>>s;
          int f=0;
-         for(int i=0;i<n;i++)
-         {
-              if(s[i]=='B')
-              {
+         for(int i=0;i<n;i++){
+              if(s[i]=='B'){
                   f=1;
               }
          }
-         long long index=0;
-         for(int i=1;i<=mx;i++)
-         {
-             arr[i]=s[index];
-             index++;
-             if(index==n)
-             {
-                 index=0;
-             }
-         }
-         long long brr[q+10];
+         
+         ll brr[q+10];
          for(int i=1;i<=q;i++)
          {
              cin>>brr[i];
          }
-         for(int i=1;i<=q;i++)
-         {
-              long long a=brr[i],sum=0;
-              if(f==0)
-              {
-                  cout<<a<<endl;
+         for(int i=1;i<=q;i++){
+              ll a=brr[i],sum=0;
+              if(f==0){
+                  pr(a);
                   continue;
               }
               for(int j=1;j<=mx;j++)
               {
-                    if(arr[j]=='A')
-                    {
+                    if(s[j%n]=='A'){
                         a--;
                         sum++;
                     }
-                    else
-                    {
+                    else{
                         a=a/2;
                         sum++;
                     }
-                    if(a<=0)
-                    {
-                        break;
-                    }
+                    if(a<=0)break;
               }
-              cout<<sum<<endl;
+              pr(sum);
          }
     }
     return 0;
