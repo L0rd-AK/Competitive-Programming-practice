2eaa55fd08da3d551aa283612826f987e71890c6
l0rd-AK <aka883207@gmail.com>
2025-11-07 23:34:05 +0600

    Fix input data in input.txt and correct string indexing in AKG function

--- test.cpp
+++ test.cpp
@@ -29,8 +29,8 @@ void AKG(){
     string s;cin>>s;
     int n=s.length();
     for(int i=0;i<n;){
-        if(i<n-2 && s[i]=='1'&&s[i]=='4'&&s[i]=='4')i+=3;
-        else if(i<n-1 && s[i]=='1'&&s[i]=='4')i+=2;
+        if(i<n-2 && s[i]=='1'&&s[i+1]=='4'&&s[i+2]=='4')i+=3;
+        else if(i<n-1 && s[i]=='1'&&s[i+1]=='4')i+=2;
         else if(s[i]=='1')i++;
         else{
             yn(0);
