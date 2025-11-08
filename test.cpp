b526d4351c03829317e98d32267528509187b40c
l0rd-AK <aka883207@gmail.com>
2025-11-08 22:21:56 +0600

    Refactor AKG function to read two integers instead of processing a string

--- test.cpp
+++ test.cpp
@@ -26,18 +26,7 @@ const int MOD = 1e9+7;
 ll N = 1e18;
 
 void AKG(){
-    string s;cin>>s;
-    int n=s.length();
-    for(int i=0;i<n;){
-        if(i<n-2 && s[i]=='1'&&s[i+1]=='4'&&s[i+2]=='4')i+=3;
-        else if(i<n-1 && s[i]=='1'&&s[i+1]=='4')i+=2;
-        else if(s[i]=='1')i++;
-        else{
-            yn(0);
-            return;
-        }
-    }
-    yn(1);
+    int n,m;cin>>n>>m;
 }
 
 int main() {
