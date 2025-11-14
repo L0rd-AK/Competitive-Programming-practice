a77c5b8d7c071043304e15197d8b0e4c2ea4ab5c
l0rd-AK <aka883207@gmail.com>
2025-11-14 17:23:08 +0600

    Refactor input handling in testy.cpp; remove local debugging code and clean up formatting

--- testy.cpp
+++ testy.cpp
@@ -1,9 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#ifndef ONLINE_JUDGE
-#define LOCAL
-#endif
 
 #define ll long long
 #define f(x1, y1, z1) for (int x1 = y1; x1 < z1; x1++)
@@ -16,19 +13,11 @@ using namespace std;
 #define pb push_back
 #define pi pair<ll, ll>
 
-#ifdef LOCAL
-#include "D:\\VS-Code\\Competitive programming practice\\Zest-cases\\dbg.h"
-#else
-#define dbg(x...)
-#endif
 
 const int MOD = 1e9+7;
 ll N = 1e18;
 
 int main() {
-#ifdef LOCAL
-    freopen("D:\\VS-Code\\Competitive programming practice\\input.txt", "r", stdin);
-#endif
 
     ios_base::sync_with_stdio(0);
     cin.tie(0);
@@ -43,26 +32,26 @@ int main() {
         priority_queue<int> maxPQ;
         maxPQ.push(a[0]);
         v[0].first = true;
-        f(i, 1, n) {
-            if (a[i] > maxPQ.top()) v[i].first = true;
+        f(i,1,n) {
+            if (a[i]>maxPQ.top())v[i].first=true;
             maxPQ.push(a[i]);
         }
         priority_queue<int, vector<int>, greater<int>> minPQ;
-        minPQ.push(a[n - 1]);
-        v[n - 1].second = true;
-        for (int i = n - 2; i >= 0; i--) {
+        minPQ.push(a[n-1]);
+        v[n-1].second = true;
+        for (int i=n-2; i>=0;i--) {
             if (a[i] < minPQ.top()) v[i].second = true;
             minPQ.push(a[i]);
         }
         int idx = 1; bool flg = 0;
         f(k, 0, n) {
             if (v[k].first && v[k].second) {
-                cout << "Case " << (j + 1) << ": " << idx << endl;
+                cout<<"Case "<<(j+1)<<": "<<idx<<endl;
                 flg = 1; break;
             }
             idx++;
         }
-        if (!flg) cout << "Case " << (j + 1) << ": " << "Humanity is doomed!" << endl;
+        if (!flg)cout<<"Case "<<(j+1)<<": "<<"Humanity is doomed!"<<endl;
     }
     return 0;
 }
