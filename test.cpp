342b52b4bc2e888778ad13968b3da412d6848036
l0rd-AK <aka883207@gmail.com>
2025-11-20 23:55:21 +0600

    Refactor AKG function to streamline input handling and character counting logic

--- test.cpp
+++ test.cpp
@@ -26,7 +26,13 @@ const int MOD = 1e9+7;
 ll N = 1e18;
 
 void AKG(){
-    
+        string s;
+		cin>>s;
+		int cnt[2]={0,0},ans=0;
+		for(char c:s)++cnt[c-'0'];
+		for(char c:s)if(!cnt[(c-'0')^1]--)cout<<s.size()-ans<<"\n";
+		else ++ans;
+		if(ans==s.size())cout<<0<<"\n";
 }
 
 int main() {
