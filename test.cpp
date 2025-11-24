d137d03b6ecf2ba2e728aeb252ca94364b1072f7
l0rd-AK <aka883207@gmail.com>
2025-11-24 23:59:47 +0600

    Remove unused input handling and output logic from AKG function

--- test.cpp
+++ test.cpp
@@ -26,13 +26,7 @@ const int MOD = 1e9+7;
 ll N = 1e18;
 
 void AKG(){
-    int n;cin>>n;
-    int x=(1<<30)-1;
-    while(n--){
-        int a;
-        cin>>a,x&=a;
-    }
-    pr(x);
+  
 }
 
 int main() {
