c0e3443663007f73b1755b730db03d42e3e13331
l0rd-AK <aka883207@gmail.com>
2025-11-30 23:27:58 +0600

    Add initial implementation of the AKG function for problem 2170A

---
+++ CodeForces-Problems/2170A.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifndef ONLINE_JUDGE
+#define LOCAL
+#endif
+
+#define ll long long
+#define f(x1, y1, z1) for (int x1 = y1; x1 < z1; x1++)
+#define endl "\n"
+#define yn(f) cout << (f ? "YES\n" : "NO\n")
+#define pr(x) cout << x << "\n"
+#define all(x) x.begin(), x.end()
+#define vl vector<ll>
+#define vi vector<int>
+#define pb push_back
+#define pi pair<ll, ll>
+
+#ifdef LOCAL
+#include "D:\\VS-Code\\Competitive programming practice\\Zest-cases\\dbg.h"
+#else
+#define dbg(x...)
+#endif
+
+const int MOD = 1e9+7;
+ll N = 1e18;
+
+void AKG(){
+    ll n;cin >> n;
+    ll ans;
+    if(n==1)ans = 1;
+    else if(n==2)ans = 9;                       
+    else if (n == 3 || n == 4)ans = 4*n*n - n - 4;
+    else ans = 5*n*n - 5*n - 5;         
+    pr(ans);
+}
+
+int main() {
+#ifdef LOCAL
+    freopen("D:\\VS-Code\\Competitive programming practice\\input.txt", "r", stdin);
+#endif
+
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    int t=1;
+    cin >> t; 
+    while(t--)AKG();
+    return 0;
+}
