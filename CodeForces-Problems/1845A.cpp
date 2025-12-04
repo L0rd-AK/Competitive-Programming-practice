617e9d9706e089d94d7734f600bbd30b6b2c135f
l0rd-AK <aka883207@gmail.com>
2025-12-04 23:56:56 +0600

    codeforces done

---
+++ CodeForces-Problems/1845A.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifndef ONLINE_JUDGE
+#define LOCAL
+#endif
+
+#define ll long long
+#define f(x1, y1, z1) for (int x1 = y1; x1 < z1; x1++)
+#define endl "\n"
+#define yn(f) cout << (f ? "YES\n" : "NO\n")
+#define pr(x) cout << x << "\n"
+#define all(x) x.begin(), x.end()
+#define vl vector<ll>
+#define vi vector<int>
+#define pb push_back
+#define pi pair<ll, ll>
+
+#ifdef LOCAL
+#include "D:\\VS-Code\\Competitive programming practice\\Zest-cases\\dbg.h"
+#else
+#define dbg(x...)
+#endif
+
+const int MOD = 1e9+7;
+ll N = 1e18;
+
+void AKG(){
+    int n,k,x; cin>>n>>k>>x;
+	if(x!=1){
+		yn(1);
+		cout<<n<<'\n';
+		for(int i=1;i<=n;i++) cout<<1<<" ";
+		cout << '\n';
+	}
+	else if((n%2 && k==2) || k==1) cout<<"NO\n";
+	else{
+		yn(1);
+		cout<<n/2<<'\n';
+		for(int i=1;i<n/2;i++) cout<<2<<" ";
+		cout<<2+(n%2)<<'\n';
+	}
+}
+
+int main() {
+#ifdef LOCAL
+    freopen("D:\\VS-Code\\Competitive programming practice\\input.txt", "r", stdin);
+#endif
+
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    int t=1;
+    cin >> t; 
+    while(t--)AKG();
+    return 0;
+}
