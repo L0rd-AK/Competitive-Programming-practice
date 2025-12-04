200edec3389f675b141dd10b60930f0b4842a0dc
l0rd-AK <aka883207@gmail.com>
2025-12-04 23:53:45 +0600

    Update AKG function to enhance output formatting and handle additional cases

--- test.cpp
+++ test.cpp
@@ -29,11 +29,15 @@ void AKG(){
     int n,k,x;cin>>n>>k>>x;
     if(k>2&&x!=1){
         yn(1);
-        cout<<"1\n"<<n<<endl;
+        cout<<n<<'\n';
+		for(int i=1;i<=n;i++) cout<<1<<" ";
+		cout << '\n';
     }
     else if(k==2&&x==1&&n%2==0){
         yn(1);
-        cout<<"2\n"<<n/2<<endl;
+        cout<<n/2<<'\n';
+		for(int i=1;i<n/2;i++) cout<<2<<" ";
+		cout<<2+(n%2)<<'\n';
     }
     else yn(0);
 }
