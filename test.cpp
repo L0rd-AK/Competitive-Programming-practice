8b522051f5bbe81136eb00d888534ecabbd72f96
l0rd-AK <aka883207@gmail.com>
2025-12-04 23:56:16 +0600

    Refactor AKG function to improve case handling and output for k and x conditions

--- test.cpp
+++ test.cpp
@@ -26,20 +26,20 @@ const int MOD = 1e9+7;
 ll N = 1e18;
 
 void AKG(){
-    int n,k,x;cin>>n>>k>>x;
-    if(k>2&&x!=1){
-        yn(1);
-        cout<<n<<'\n';
+    int n,k,x; cin>>n>>k>>x;
+	if(x!=1){
+		yn(1);
+		cout<<n<<'\n';
 		for(int i=1;i<=n;i++) cout<<1<<" ";
 		cout << '\n';
-    }
-    else if(k==2&&x==1&&n%2==0){
-        yn(1);
-        cout<<n/2<<'\n';
+	}
+	else if((n%2 && k==2) || k==1) cout<<"NO\n";
+	else{
+		yn(1);
+		cout<<n/2<<'\n';
 		for(int i=1;i<n/2;i++) cout<<2<<" ";
 		cout<<2+(n%2)<<'\n';
-    }
-    else yn(0);
+	}
 }
 
 int main() {
