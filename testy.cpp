5d0f66b0bff39465c9ef674210512ac8dfa1a085
l0rd-AK <aka883207@gmail.com>
2025-12-18 23:23:16 +0600

    Refactor main function in testy.cpp for improved clarity and performance

--- testy.cpp
+++ testy.cpp
@@ -1,25 +1,36 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main()
-{
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    int t;
-    cin >> t;
-    while (t--)
-    {
-        int n;
-        cin >> n;
-        bitset<1005> b;
-        while (n--)
-        {
-            int x;
-            cin >> x;
-            b.set(x);
+typedef long long ll;
+typedef pair<ll,ll> pll;
+typedef pair<int,int> pii;
+#define f first
+#define S second
+#define pb push_back
+#define el '\n'
+
+
+
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    freopen("D:\\VS-Code\\Competitive programming practice\\input.txt", "r", stdin);
+
+int t; cin>>t;
+    while (t--) {
+        int n,a; cin>>n>>a;
+        vector<int> v(n);
+        int cnt1=0,cnt2=0;
+        for (int i = 0; i < n; i++) cin>>v[i];
+        sort(v.begin(),v.end());
+        for (int i=0;i<n;i++) {
+            if (v[i]<=a)cnt1++;
+            if (v[i]>=a)cnt2++;
         }
-        int c = b.count();
-        while (!b.test(c))
-            b.set(c++);
-        cout << c << "\n";
+        if (cnt1<cnt2)cout<<a+1<<el;
+        else cout<<a-1<<el;
     }
+   return 0;
+
+
+
 }
