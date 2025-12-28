b1a627f5f88d46eda1fb348b58eb328880aa9a2e
l0rd-AK <aka883207@gmail.com>
2025-12-28 23:54:41 +0600

    Refactor main function and update AKG function for improved input handling and logic

--- testy.cpp
+++ testy.cpp
@@ -1,36 +1,56 @@
 #include <bits/stdc++.h>
 using namespace std;
-typedef long long ll;
-typedef pair<ll,ll> pll;
-typedef pair<int,int> pii;
-#define f first
-#define S second
+
+#ifndef ONLINE_JUDGE
+#define LOCAL
+#endif
+
+#define ll long long
+#define f(x1, y1, z1) for (int x1 = y1; x1 < z1; x1++)
+#define endl "\n"
+#define yn(f) cout << (f ? "YES\n" : "NO\n")
+#define pr(x) cout << x << "\n"
+#define all(x) x.begin(), x.end()
+#define vl vector<ll>
+#define vi vector<int>
 #define pb push_back
-#define el '\n'
+#define pi pair<ll, ll>
 
+#ifdef LOCAL
+#include "D:\\VS-Code\\Competitive programming practice\\Zest-cases\\dbg.h"
+#else
+#define dbg(x...)
+#endif
 
+const int MOD = 1e9+7;
+ll N = 1e18;
 
+void AKG(){
+    int n,k;cin>>n>>k;
+    string s;cin>>s;
+    int ans=-1,c=0;
+    if(s[0]=='B')c++;
+    f(i,0,n-1){
+        if(s[i]=='B'&&s[i+1]=='B')c++;
+        else{
+            ans=max(c,ans);
+        }
+    }
+    ans=max(c,ans);
+    if(ans>=k)pr(0);
+    else pr(k-ans);
+}
 
 int main() {
-    ios_base::sync_with_stdio(false); cin.tie(NULL);
+#ifdef LOCAL
     freopen("D:\\VS-Code\\Competitive programming practice\\input.txt", "r", stdin);
+#endif
 
-int t; cin>>t;
-    while (t--) {
-        int n,a; cin>>n>>a;
-        vector<int> v(n);
-        int cnt1=0,cnt2=0;
-        for (int i = 0; i < n; i++) cin>>v[i];
-        sort(v.begin(),v.end());
-        for (int i=0;i<n;i++) {
-            if (v[i]<=a)cnt1++;
-            if (v[i]>=a)cnt2++;
-        }
-        if (cnt1<cnt2)cout<<a+1<<el;
-        else cout<<a-1<<el;
-    }
-   return 0;
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
 
-
-
+    int t=1;
+    cin >> t; 
+    while(t--)AKG();
+    return 0;
 }
