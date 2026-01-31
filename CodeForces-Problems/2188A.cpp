8817d56b9e11235e15f7d7325628344b2ba28844
l0rd-AK <aka883207@gmail.com>
2026-01-31 23:59:30 +0600

    Add AKG function implementation for array population and output formatting

---
+++ CodeForces-Problems/2188A.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifndef ONLINE_JUDGE
+#define LOCAL
+#endif
+
+#define ll long long
+#define f(x1, y1, z1) for (int x1 = y1; x1 < z1; x1++)
+#define endl "\n"
+#define yn(f) cout << (f ? "YES\n" : "NO\n")
+#define pr(x) cout << x << "\n"
+#define all(x) x.begin(), x.end()
+#define vl vector<ll>
+#define vi vector<int>
+#define pb push_back
+#define pi pair<ll, ll>
+
+#ifdef LOCAL
+#include "D:\\VS-Code\\Competitive programming practice\\Zest-cases\\dbg.h"
+#else
+#define dbg(x...)
+#endif
+
+const int MOD = 1e9+7;
+ll N = 1e18;
+
+void AKG(){
+    int n;cin >> n;
+    vi a(n);
+    int l=1;
+    int r=n;
+    for(int i=n-1;i>=0;i-=2){
+        a[i]=r--;
+        if(i-1>=0)
+        a[i-1]=l++;
+    }
+    f(i,0,n)cout<<a[i]<<" ";
+    cout<<endl;
+
+}
+
+int main() {
+#ifdef LOCAL
+    freopen("D:\\VS-Code\\Competitive programming practice\\input.txt", "r", stdin);
+#endif
+
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    int t=1;
+    cin >> t; 
+    while(t--)AKG();
+    return 0;
+}
