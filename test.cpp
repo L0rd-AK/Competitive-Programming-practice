2e0823b40d0d09b8f1f8fd3e8511b4c4f2ed5c48
l0rd-AK <aka883207@gmail.com>
2026-03-22 23:58:57 +0600

    Implement AKG function logic for competitive programming challenge

--- test.cpp
+++ test.cpp
@@ -26,7 +26,24 @@ const int MOD = 1e9+7;
 ll N = 1e18;
 
 void AKG(){
-  
+  int n;cin >> n;
+  string s;cin >> s;
+  s = '1' + s + '1';
+  int ans = 0;
+    for (int i = 1, l = 0; i <= n; ++i){
+        if (s[i] == '0') {
+            if (s[i-1] == '1') {
+                l = i;
+            }
+            if (s[i+1] == '1') {
+                int c = (l == 1) + (i == n);
+                ans += (i - l + 1 + c) / 3;
+            }
+        } else {
+            ++ans;
+        }
+    }
+  pr(ans);
 }
 
 int main() {
