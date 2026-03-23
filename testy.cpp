6ead7cad92165b5217acbc9b9887f71b4b71e92b
l0rd-AK <aka883207@gmail.com>
2026-03-23 23:59:02 +0600

    Refactor AKG function to improve input handling and processing logic

--- testy.cpp
+++ testy.cpp
@@ -1,30 +1,60 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main()
-{
-    int t;
-    cin >> t;
-    while (t--)
-    {
-        int n;
-        cin >> n;
-        int mn1 = (1e9), mn2 = (1e9), m;
-        long long sum = 0;
-        while (n--)
-        {
-            cin >> m;
-            int a[m];
-            for (int i = 0; i < m; i++)
-            {
-                cin >> a[i];
-            }
-            sort(a, a + m);
-            sum += a[1];
-            if (mn1 > a[0])
-                mn1 = a[0];
-            if (mn2 > a[1])
-                mn2 = a[1];
-        }
-        cout << sum - mn2 + mn1 << endl;
+
+#ifndef ONLINE_JUDGE
+#define LOCAL
+#endif
+
+#define ll long long
+#define f(x1, y1, z1) for (int x1 = y1; x1 < z1; x1++)
+#define endl "\n"
+#define yn(f) cout << (f ? "YES\n" : "NO\n")
+#define pr(x) cout << x << "\n"
+#define all(x) x.begin(), x.end()
+#define vl vector<ll>
+#define vi vector<int>
+#define pb push_back
+#define pi pair<ll, ll>
+
+#ifdef LOCAL
+#include "D:\\VS-Code\\Competitive programming practice\\Zest-cases\\dbg.h"
+#else
+#define dbg(x...)
+#endif
+
+const int MOD = 1e9+7;
+ll N = 1e18;
+
+void AKG() {
+    ll n, c, k;
+    cin >> n >> c >> k;
+    vl a(n);
+    f(i, 0, n) cin >> a[i];
+
+    sort(all(a));
+
+    f(i, 0, n) {
+        if (a[i] > c) break;
+
+        ll use = min(k, c - a[i]);
+        c += a[i] + use;
+        k -= use;
     }
+
+    pr(c);
+}
+
+int main() {
+#ifdef LOCAL
+    freopen("D:\\VS-Code\\Competitive programming practice\\input.txt", "r", stdin);
+#endif
+
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    int t = 1;
+    cin >> t;
+    while (t--) AKG();
+
+    return 0;
 }
