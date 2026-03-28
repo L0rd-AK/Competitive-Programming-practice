7fd1ab918549e77813a57987cf5000c18d40c4bf
l0rd-AK <aka883207@gmail.com>
2026-03-28 21:08:35 +0600

    Refactor HTML and CSS for flower animation; update JavaScript for star field and sparkles. Simplify C++ code structure and remove unnecessary macros.

--- testy.cpp
+++ testy.cpp
@@ -1,52 +1,31 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
 using namespace std;
-
-#ifndef ONLINE_JUDGE
-#define LOCAL
-#endif
-
-#define ll long long
-#define f(x1, y1, z1) for (int x1 = y1; x1 < z1; x1++)
-#define endl "\n"
-#define yn(f) cout << (f ? "YES\n" : "NO\n")
-#define pr(x) cout << x << "\n"
-#define all(x) x.begin(), x.end()
-#define vl vector<ll>
-#define vi vector<int>
-#define pb push_back
-#define pi pair<ll, ll>
-
-#ifdef LOCAL
-#include "D:\\VS-Code\\Competitive programming practice\\Zest-cases\\dbg.h"
-#else
-#define dbg(x...)
-#endif
-
-const int MOD = 1e9+7;
-ll N = 1e18;
-
-void AKG() {
-    int n;cin>>n;
-    vi a(n);
-    f(i,0,n)cin>>a[i];
-    int ans=0,x=a[0];
-    f(i,1,n){
-        if(x<=a[i]){ans++;x=a[i];}
-    }
-    pr(ans+1);
-}
-
-int main() {
-#ifdef LOCAL
-    freopen("D:\\VS-Code\\Competitive programming practice\\input.txt", "r", stdin);
-#endif
-
-    ios_base::sync_with_stdio(0);
+int main()
+{
+    ios::sync_with_stdio(0);
     cin.tie(0);
-
-    int t = 1;
+    int t;
     cin >> t;
-    while (t--) AKG();
-
-    return 0;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        vector<long long> a(n);
+        for (int i = 0; i < n; i++)
+            cin >> a[i];
+        for (int i = 0; i < n; i++)
+        {
+            int l = 0, r = 0;
+            for (int j = i + 1; j < n; j++)
+            {
+                if (a[j] < a[i])
+                    l++;
+                if (a[j] > a[i])
+                    r++;
+            }
+            cout << (l > r ? l : r) << " ";
+        }
+        cout << "\n";
+    }
 }
