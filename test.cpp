d67bf31edefdf2b829f064712ee764e8ad42902a
l0rd-AK <aka883207@gmail.com>
2026-03-30 22:51:49 +0600

    Remove Solution class and searchInsert function from test.cpp

--- test.cpp
+++ test.cpp
@@ -1,18 +1,5 @@
 #include <bits/stdc++.h>
 using namespace std;
-class Solution {
-public:
-    int searchInsert(vector<int>& nums, int target) {
-        int l=0,r=nums.size()-1;
-		while(l<=r){
-			int m=(l+r)/2;
-			if(nums[m]==target)return m;
-			else if(nums[m]<target)l=m+1;
-			else r=m-1;
-		}
-		return l;
-    }
-};
 
 #ifndef ONLINE_JUDGE
 #define LOCAL
