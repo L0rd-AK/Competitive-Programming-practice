b585860bb39f1eeed6cbee16f8ff0e514001adab
l0rd-AK <aka883207@gmail.com>
2026-04-06 22:43:51 +0600

    Add implementation of recoverOrder function to filter friends from order

---
+++ LeetCode/Restore_finishing_order_3668.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    vector<int> recoverOrder(vector<int>& order, vector<int>& friends) {
+        vector<int> ans;
+		map<int,int> mp;
+		for(int i:friends)mp[i]++;
+		for(int i:order)if(mp.count(i)>0)ans.push_back(i);
+		//reverse(ans.begin(),ans.end());
+		return ans;
+    }
+};
