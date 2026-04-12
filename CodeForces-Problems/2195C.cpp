878915eb0cdcab7ecebb53eada0fb27b756aab31
l0rd-AK <aka883207@gmail.com>
2026-04-12 22:59:50 +0600

    Add initial implementation of AKG function for input processing and output calculation

---
+++ CodeForces-Problems/2195C.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define f(x1, y1, z1) for (int x1 = y1; x1 < z1; x1++)
+#define endl "\n"
+#define yn(f) cout << (f ? "YES\n" : "NO\n")
+#define pr(x) cout << x << "\n"
+#define all(x) x.begin(), x.end()
+#define vl vector<ll>
+#define vi vector<int>
+#define pb push_back
+#define pi pair<ll, ll>
+
+const int MOD = 1e9+7;
+ll N = 1e18;
+
+void AKG(){
+    int n;
+    cin >> n;
+    vi a(n);
+    f(i, 0, n) cin >> a[i];
+
+    int ans = 0;
+    f(i, 1, n){
+        if(a[i] == a[i-1] || a[i] + a[i-1] == 7) ans++,i++;
+    }
+
+    pr(ans);
+}
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    int t = 1;
+    cin >> t;
+    while(t--) AKG();
+    return 0;
+}
