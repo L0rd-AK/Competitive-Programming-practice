9fe6739a9daf906686b9d95509acfdf94be60f69
l0rd-AK <aka883207@gmail.com>
2026-05-06 23:07:46 +0600

    Add implementation of twoEditWords function to find words within two edits of a dictionary

---
+++ LeetCode/Words_Within_Two_Edits_of_Dictionary_2452.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<string> twoEditWords(vector<string>& queries, vector<string>& dictionary) {
+        vector<string> ans;
+        
+        for (const string& qu : queries) {
+            for (const string& dk : dictionary) {
+                int c = 0;
+                for (int i = 0; i < qu.length(); ++i) {
+                    if(qu[i]!=dk[i])c++;
+                    if(c>2) break; 
+                }
+                
+                if(c<=2){
+                    ans.push_back(qu);
+                    break; 
+                }
+            }
+        }
+        
+        return ans;
+    }
+};
