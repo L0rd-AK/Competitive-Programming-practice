92d2b10a0a3851bd27d260c486445cf84b4e7e34
l0rd-AK <aka883207@gmail.com>
2026-06-10 23:58:14 +0600

    Add initial implementation of AKG function for competitive programming

---
+++ CodeForces-Problems/2234A.cpp
@@ -0,0 +1,58 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define f(x1, y1, z1) for (int x1 = y1; x1 < z1; x1++)
+#define endl "\n"
+#define yn(f) cout << (f ? "YES\n" : "NO\n")
+#define pr(x) cout << x << "\n"
+#define all(x) x.begin(), x.end()
+#define vl vector<ll>
+#define vi vector<int>
+#define pb push_back
+#define pi pair<ll, ll>
+
+const int MOD = 1e9+7;
+ll N = 1e18;
+
+void AKG(){
+	ll n; cin >> n;
+	vi a(n);
+	f(i, 0, n) cin >> a[i];
+	
+	sort(all(a), greater<int>());
+	
+	if (n == 2) {
+		cout << a[0] << " " << a[1] << endl;
+		return;
+	}
+	
+	f(i, 0, n - 1) {
+		if (a[i] <= a[i+1]) {
+			pr(-1);
+			return;
+		}
+	}
+	
+	f(i, 0, n - 2) {
+		if (a[i+2] != a[i] % a[i+1]) {
+			pr(-1);
+			return;
+		}
+	}
+	
+	cout << a[0] << " " << a[1] << endl;
+}
+
+
+int main() {
+
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    int t=1;
+    cin >> t; 
+    while(t--)AKG();
+    return 0;
+}
