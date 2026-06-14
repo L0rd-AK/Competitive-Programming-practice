4fb04c4f6d0459b9ff72d59231b68dd7c6bf0988
l0rd-AK <aka883207@gmail.com>
2026-06-14 22:08:40 +0600

    Add solution for Left and Right Sum Differences problem

---
+++ CodeForces-Problems/Left_and_Right_Sum_Differences_2574.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> leftRightDifference(vector<int>& nums) {
+        int n=nums.size();
+        vector<int> l(n),r(n);
+        l[0]=nums[0];
+        r[n-1]=nums[n-1];
+
+        for(int i=1;i<n;i++)l[i]=l[i-1]+nums[i];
+        for(int i=n-2;i>=0;i--)r[i]=r[i+1]+nums[i];
+        vector<int> ans(n);
+        for(int i=0;i<n;i++)ans[i]=abs(l[i]-r[i]);
+        return ans;
+    }
+};
